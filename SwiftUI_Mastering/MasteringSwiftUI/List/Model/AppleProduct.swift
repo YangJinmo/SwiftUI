8f6cd7a181053de188bb503cf3b1692c38969c07
zzimss <didwlsah9@gmail.com>
2023-02-26 23:43:42 +0900

    Re-indent

--- SwiftUI_Mastering/MasteringSwiftUI/List/Model/AppleProduct.swift
+++ SwiftUI_Mastering/MasteringSwiftUI/List/Model/AppleProduct.swift
@@ -24,131 +24,155 @@
 import Foundation
 
 struct AppleProduct: Identifiable, Hashable {
-  let id = UUID()
-  let name: String
-  let summary: String
-  let category: String
-  let price: Int
+    let id = UUID()
+    let name: String
+    let summary: String
+    let category: String
+    let price: Int
 }
 
 extension AppleProduct {
-  static var sampleList: [AppleProduct] {
-    return [
-      AppleProduct(name: "MacBook Air",
-                   summary: """
-                        1.6GHz 듀얼 코어 프로세서(최대 3.6GHz Turbo Boost)
-                        256GB 저장 용량
-                        Touch ID
-                        """,
-                   category: "Mac",
-                   price: 1_740_000),
-      AppleProduct(name: "MacBook Pro",
-                   summary: """
-                        2.3GHz 8코어 프로세서(최대 4.8GHz Turbo Boost)
-                        512GB 저장 용량
-                        Touch Bar 및 Touch ID
-                        """,
-                   category: "Mac",
-                   price: 3_490_000),
-      AppleProduct(name: "iMac Retina 5K",
-                   summary: """
-                        3.7GHz 6코어 프로세서(최대 4.6GHz Turbo Boost)
-                        2TB 저장 용량
-                        Retina 5K 디스플레이
-                        """,
-                   category: "Mac",
-                   price: 2_950_000),
-      AppleProduct(name: "iMac Pro",
-                   summary: """
-                        3.2GHz 8코어 Intel Xeon W 프로세서 (최대 4.2GHz Turbo Boost)
-                        1TB SSD 저장 장치
-                        27형 Retina 5K 5120 x 2880 P3 디스플레이
-                        """,
-                   category: "Mac",
-                   price: 6_300_000),
-      AppleProduct(name: "Mac mini",
-                   summary: """
-                        3.0GHz 6코어 프로세서(최대 4.1GHz Turbo Boost)
-                        256GB 저장 용량
-                        """,
-                   category: "Mac",
-                   price: 1_450_000),
-      AppleProduct(name: "iPad Pro 12.9\"",
-                   summary: """
-                        실버 / 스페이스 그레이
-                        1TB
-                        Wi-Fi + Cellular
-                        """,
-                   category: "iPad",
-                   price: 2_470_000),
-      AppleProduct(name: "iPad Air",
-                   summary: """
-                        실버 / 스페이스 그레이 / 골드
-                        256GB
-                        Wi-Fi + Cellular
-                        """,
-                   category: "iPad",
-                   price: 999_000),
-      AppleProduct(name: "iPad",
-                   summary: """
-                        실버 / 스페이스 그레이 / 골드
-                        128GB
-                        Wi-Fi + Cellular
-                        """,
-                   category: "iPad",
-                   price: 720_000),
-      AppleProduct(name: "Apple Pencil",
-                   summary: """
-                        호환 기기:
-                        12.9형 iPad Pro(3세대)
-                        11형 iPad Pro
-                        """,
-                   category: "iPad",
-                   price: 159_000),
-      AppleProduct(name: "iPhone Xs",
-                   summary: """
-                        5.8" Super Retina HD 디스플레이
-                        실버 / 스페이스 그레이 / 골드
-                        A12 Bionic 칩
-                        256GB
-                        듀얼 12MP 와이드 앵글 및 망원 카메라, TrueDepth 카메라
-                        Face ID
-                        """,
-                   category: "iPhone",
-                   price: 1_580_000),
-      AppleProduct(name: "iPhone Xs Max",
-                   summary: """
-                        6.5" Super Retina HD 디스플레이
-                        실버 / 스페이스 그레이 / 골드
-                        A12 Bionic 칩
-                        256GB
-                        듀얼 12MP 와이드 앵글 및 망원 카메라, TrueDepth 카메라
-                        Face ID
-                        """,
-                   category: "iPhone",
-                   price: 1_710_000),
-      AppleProduct(name: "iPhone Xr",
-                   summary: """
-                        6.1" Liquid Retina HD 디스플레이
-                        화이트 / 블랙 / 블루 / 옐로 / 코럴
-                        A12 Bionic 칩
-                        256GB
-                        12MP 와이드 앵글, TrueDepth 카메라
-                        Face ID
-                        """,
-                   category: "iPhone",
-                   price: 1_200_000),
-      
-      AppleProduct(name: "Apple Watch Series 4",
-                   summary: """
-                        44mm
-                        실버 알루미늄 케이스와 화이트
-                        스포츠 밴드
-                        GPS + Cellular
-                        """,
-                   category: "Apple Watch",
-                   price: 659_000)
-    ]
-    
-  }
+    static var sampleList: [AppleProduct] {
+        return [
+            AppleProduct(
+                name: "MacBook Air",
+                summary: """
+                1.6GHz 듀얼 코어 프로세서(최대 3.6GHz Turbo Boost)
+                256GB 저장 용량
+                Touch ID
+                """,
+                category: "Mac",
+                price: 1740000
+            ),
+            AppleProduct(
+                name: "MacBook Pro",
+                summary: """
+                2.3GHz 8코어 프로세서(최대 4.8GHz Turbo Boost)
+                512GB 저장 용량
+                Touch Bar 및 Touch ID
+                """,
+                category: "Mac",
+                price: 3490000
+            ),
+            AppleProduct(
+                name: "iMac Retina 5K",
+                summary: """
+                3.7GHz 6코어 프로세서(최대 4.6GHz Turbo Boost)
+                2TB 저장 용량
+                Retina 5K 디스플레이
+                """,
+                category: "Mac",
+                price: 2950000
+            ),
+            AppleProduct(
+                name: "iMac Pro",
+                summary: """
+                3.2GHz 8코어 Intel Xeon W 프로세서 (최대 4.2GHz Turbo Boost)
+                1TB SSD 저장 장치
+                27형 Retina 5K 5120 x 2880 P3 디스플레이
+                """,
+                category: "Mac",
+                price: 6300000
+            ),
+            AppleProduct(
+                name: "Mac mini",
+                summary: """
+                3.0GHz 6코어 프로세서(최대 4.1GHz Turbo Boost)
+                256GB 저장 용량
+                """,
+                category: "Mac",
+                price: 1450000
+            ),
+            AppleProduct(
+                name: "iPad Pro 12.9\"",
+                summary: """
+                실버 / 스페이스 그레이
+                1TB
+                Wi-Fi + Cellular
+                """,
+                category: "iPad",
+                price: 2470000
+            ),
+            AppleProduct(
+                name: "iPad Air",
+                summary: """
+                실버 / 스페이스 그레이 / 골드
+                256GB
+                Wi-Fi + Cellular
+                """,
+                category: "iPad",
+                price: 999000
+            ),
+            AppleProduct(
+                name: "iPad",
+                summary: """
+                실버 / 스페이스 그레이 / 골드
+                128GB
+                Wi-Fi + Cellular
+                """,
+                category: "iPad",
+                price: 720000
+            ),
+            AppleProduct(
+                name: "Apple Pencil",
+                summary: """
+                호환 기기:
+                12.9형 iPad Pro(3세대)
+                11형 iPad Pro
+                """,
+                category: "iPad",
+                price: 159000
+            ),
+            AppleProduct(
+                name: "iPhone Xs",
+                summary: """
+                5.8" Super Retina HD 디스플레이
+                실버 / 스페이스 그레이 / 골드
+                A12 Bionic 칩
+                256GB
+                듀얼 12MP 와이드 앵글 및 망원 카메라, TrueDepth 카메라
+                Face ID
+                """,
+                category: "iPhone",
+                price: 1580000
+            ),
+            AppleProduct(
+                name: "iPhone Xs Max",
+                summary: """
+                6.5" Super Retina HD 디스플레이
+                실버 / 스페이스 그레이 / 골드
+                A12 Bionic 칩
+                256GB
+                듀얼 12MP 와이드 앵글 및 망원 카메라, TrueDepth 카메라
+                Face ID
+                """,
+                category: "iPhone",
+                price: 1710000
+            ),
+            AppleProduct(
+                name: "iPhone Xr",
+                summary: """
+                6.1" Liquid Retina HD 디스플레이
+                화이트 / 블랙 / 블루 / 옐로 / 코럴
+                A12 Bionic 칩
+                256GB
+                12MP 와이드 앵글, TrueDepth 카메라
+                Face ID
+                """,
+                category: "iPhone",
+                price: 1200000
+            ),
+            AppleProduct(
+                name: "Apple Watch Series 4",
+                summary: """
+                44mm
+                실버 알루미늄 케이스와 화이트
+                스포츠 밴드
+                GPS + Cellular
+                """,
+                category: "Apple Watch",
+                price: 659000
+            ),
+        ]
+    }
 }
