95009ff2eeeb3656181482cba9cc0b037395bb88
zzimss <didwlsah9@gmail.com>
2023-02-27 12:26:07 +0900

    Update Alert.swift

--- SwiftUI_Mastering/MasteringSwiftUI/SystemViews/Alert.swift
+++ SwiftUI_Mastering/MasteringSwiftUI/SystemViews/Alert.swift
@@ -25,7 +25,11 @@ import SwiftUI
 
 struct View_Alert: View {
     @State private var result = ""
+
     @State private var showAlert = false
+    @State private var showImageAlert = false
+
+    @State private var imageData: ImageData? = nil
 
     var body: some View {
         VStack {
@@ -33,27 +37,70 @@ struct View_Alert: View {
                 .font(.largeTitle)
 
             Button(action: {
-                self.showAlert.toggle()
+//                showAlert.toggle()
+
+                imageData = ImageData.sample
+                showImageAlert.toggle()
             }, label: {
                 Text("Show Alert")
             })
-                .padding()
-                .alert(isPresented: $showAlert, content: {
-//          Alert(title: Text("Alert"),
-//                message: Text("Message"),
-//                dismissButton: .default(Text("OK")))
-
-                    Alert(title: Text("Alert"),
-                          message: Text("Message"),
-                          primaryButton: .destructive(Text("Delete"),
-                                                      action: {
-                                                          self.result = "Deleted"
-                                                      }),
-                          secondaryButton: .cancel({
-                              self.result = "Cancelled"
-                          }))
-                })
+            .padding()
+//            .alert(isPresented: $showAlert, content: {
+//                Alert(
+//                    title: Text("Alert"),
+//                    message: Text("Message"),
+//                    dismissButton: .default(Text("OK"))
+//                )
+//
+//                Alert(
+//                    title: Text("Alert"),
+//                    message: Text("Message"),
+//                    primaryButton: .destructive(
+//                        Text("Delete"),
+//                        action: {
+//                            self.result = "Deleted"
+//                        }
+//                    ),
+//                    secondaryButton: .cancel({
+//                        self.result = "Cancelled"
+//                    })
+//                )
+//            })
         }
+//        .alert("경고장", isPresented: $showAlert) {
+//            Button("확인") {
+//                result = "확인"
+//            }
+//
+//            Button("취소", role: .cancel) {
+//                result = "취소"
+//            }
+
+//            Button("삭제", role: .destructive) {
+//                result = "삭제"
+//            }
+
+//            Button("취소", role: .cancel) {
+//                result = "취소"
+//            }
+//        } message: {
+//            Text("조심하세요!")
+//        }
+        .alert("경고", isPresented: $showImageAlert, presenting: imageData, actions: { data in
+            Button("필터 적용") {
+                result = data.filters.joined(separator: ", ") + " 필터를 적용합니다."
+            }
+
+            Button("삭제", role: .destructive) {
+                result = "\(data.name) 삭제"
+            }
+
+            Button("취소", role: .cancel) {
+                result = "취소"
+            }
+        }, message: { data in
+            Text("\(data.name) 파일에서 어떤 작업을 할까요?\n\n\(data.date)")
+        })
     }
 }
 
