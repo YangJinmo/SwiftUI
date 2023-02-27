434e7309dee58c80f2705c8adf13f6dc65093884
zzimss <didwlsah9@gmail.com>
2023-02-27 14:08:29 +0900

    Add Animated Header

---
+++ Animated Header/Animated Header/Animated_HeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animated_HeaderApp.swift
+//  Animated Header
+//
+//  Created by Jmy on 2023/02/27.
+//
+
+import SwiftUI
+
+@main
+struct Animated_HeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
