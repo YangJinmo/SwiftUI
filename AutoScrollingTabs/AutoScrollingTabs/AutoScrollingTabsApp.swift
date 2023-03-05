5c5e207e0fb465f1216082abaa6805a7b56d9367
zzimss <didwlsah9@gmail.com>
2023-03-05 23:20:32 +0900

    Initial commit of AutoScrollingTabs

---
+++ AutoScrollingTabs/AutoScrollingTabs/AutoScrollingTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoScrollingTabsApp.swift
+//  AutoScrollingTabs
+//
+//  Created by Jmy on 2023/03/05.
+//
+
+import SwiftUI
+
+@main
+struct AutoScrollingTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoScrollingTabs/AutoScrollingTabs/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AutoScrollingTabs
+//
+//  Created by Jmy on 2023/03/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
