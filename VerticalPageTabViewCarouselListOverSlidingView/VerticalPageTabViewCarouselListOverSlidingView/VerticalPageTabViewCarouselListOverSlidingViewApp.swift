059aacd6118b0d1531f27b85066147cad12d7c71
zzimss <didwlsah9@gmail.com>
2023-03-06 16:02:02 +0900

    Initial commit of VerticalPageTabViewCarouselListOverSlidingView

---
+++ VerticalPageTabViewCarouselListOverSlidingView/VerticalPageTabViewCarouselListOverSlidingView/VerticalPageTabViewCarouselListOverSlidingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerticalPageTabViewCarouselListOverSlidingViewApp.swift
+//  VerticalPageTabViewCarouselListOverSlidingView
+//
+//  Created by Jmy on 2023/03/06.
+//
+
+import SwiftUI
+
+@main
+struct VerticalPageTabViewCarouselListOverSlidingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
