81c1dbde73526c056537f086d3ff7c920ecc206b
zzimss <didwlsah9@gmail.com>
2023-03-08 22:48:26 +0900

    Add repeating playback

--- Reels/Reels/View/CustomVideoPlayer.swift
+++ Reels/Reels/View/CustomVideoPlayer.swift
@@ -11,6 +11,10 @@ import SwiftUI
 struct CustomVideoPlayer: UIViewControllerRepresentable {
     var player: AVPlayer
 
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(parent: self)
+    }
+
     func makeUIViewController(context: Context) -> AVPlayerViewController {
         let controller = AVPlayerViewController()
         controller.player = player
@@ -18,10 +22,27 @@ struct CustomVideoPlayer: UIViewControllerRepresentable {
 
         controller.videoGravity = .resizeAspectFill
 
+        // repeating playback
+        player.actionAtItemEnd = .none
+
+        NotificationCenter.default.addObserver(context.coordinator, selector: #selector(context.coordinator.restartPlayback), name: .AVPlayerItemDidPlayToEndTime, object: player.currentItem)
+
         return controller
     }
 
     func updateUIViewController(_ playerViewController: AVPlayerViewController, context: Context) {
+    }
+
+    class Coordinator: NSObject {
+        var parent: CustomVideoPlayer
+
+        init(parent: CustomVideoPlayer) {
+            self.parent = parent
+        }
+
+        @objc func restartPlayback() {
+            parent.player.seek(to: .zero)
+        }
     }
 }
 
