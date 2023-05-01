3e970bc0d196a85a579b1945e6489c6ab22cea82
zzimss <didwlsah9@gmail.com>
2023-05-01 17:32:27 +0900

    Add CustomTransitionBetweenScreens

---
+++ CustomTransitionBetweenScreens/CustomTransitionBetweenScreens/CustomTransitionBetweenScreensApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTransitionBetweenScreensApp.swift
+//  CustomTransitionBetweenScreens
+//
+//  Created by Jmy on 2023/05/01.
+//
+
+import SwiftUI
+
+@main
+struct CustomTransitionBetweenScreensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MatchedView()
+        }
+    }
+}
