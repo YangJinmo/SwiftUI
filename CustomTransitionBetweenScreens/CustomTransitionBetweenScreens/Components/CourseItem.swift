1966b4092a5245c89a68c7864bed00c984db8853
zzimss <didwlsah9@gmail.com>
2023-05-07 14:14:54 +0900

    Add CourseItem

---
+++ CustomTransitionBetweenScreens/CustomTransitionBetweenScreens/Components/CourseItem.swift
@@ -0,0 +1,20 @@
+//
+//  CourseItem.swift
+//  CustomTransitionBetweenScreens
+//
+//  Created by Jmy on 2023/05/07.
+//
+
+import SwiftUI
+
+struct CourseItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CourseItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseItem()
+    }
+}
