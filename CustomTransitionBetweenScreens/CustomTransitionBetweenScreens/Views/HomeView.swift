92faf998c7778e55aa34e13bc98376f4baa49313
zzimss <didwlsah9@gmail.com>
2023-05-15 14:16:03 +0900

    Add HomeView, CourseView

---
+++ CustomTransitionBetweenScreens/CustomTransitionBetweenScreens/Views/HomeView.swift
@@ -0,0 +1,59 @@
+//
+//  HomeView.swift
+//  CustomTransitionBetweenScreens
+//
+//  Created by Jmy on 2023/05/15.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @State var hasScrolled = false
+    @Namespace var namespace
+    @State var show = false
+
+    var body: some View {
+        ZStack {
+            Color("Background").ignoresSafeArea()
+
+            ScrollView {
+//                scrollDetection
+
+//                featured
+                
+                Text("Courses".uppercased())
+                    .font(.footnote.weight(.semibold))
+                    .foregroundColor(.secondary)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal, 20)
+
+                if !show {
+                    CourseItem(namespace: namespace, show: $show)
+                        .onTapGesture {
+                            withAnimation(.spring(response: 0.6, dampingFraction: 0.8)) {
+                                show.toggle()
+                            }
+                        }
+                }
+
+                Color.clear.frame(height: 1000)
+            }
+            .coordinateSpace(name: "scroll")
+            .safeAreaInset(edge: .top) {
+                Color.clear.frame(height: 70)
+            }
+//            .overlay(
+//                NavigationBar(title: "Featured", hasScrolled: $hasScrolled)
+//            )
+            if show {
+                CourseView(namespace: namespace, show: $show)
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
