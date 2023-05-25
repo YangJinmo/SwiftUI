80f32c651ec1754df1861aef9009d0eed7517884
zzimss <didwlsah9@gmail.com>
2023-05-25 11:22:08 +0900

    Add WebViewApp

---
+++ WebView/WebView/WebViewApp.swift
@@ -0,0 +1,24 @@
+//
+//  WebViewApp.swift
+//  WebView
+//
+//  Created by Jmy on 2023/05/25.
+//
+
+import SwiftUI
+
+@main
+struct WebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WebViewLoadHTML(html: """
+            <html>
+            <body>
+            <h1>Hello, world!</h1>
+            <p>Welcome to my website</p>
+            </body>
+            </html>
+            """)
+        }
+    }
+}
