ec2abb2d275bef73e42db9f15ed487e96220a052
zzimss <didwlsah9@gmail.com>
2023-06-04 23:00:16 +0900

    Init commit

---
+++ Utils/Utils/UtilsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UtilsApp.swift
+//  Utils
+//
+//  Created by Jmy on 2023/06/04.
+//
+
+import SwiftUI
+
+@main
+struct UtilsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
