23c0b6280b8d7a15084a5f15fe0a7ce5ee5ba206
zzimss <didwlsah9@gmail.com>
2023-06-13 18:15:05 +0900

    Add ClearableTextField

---
+++ Utils/Utils/Views/Commons/ClearableTextField.swift
@@ -0,0 +1,46 @@
+//
+//  ClearableTextField.swift
+//  Utils
+//
+//  Created by Jmy on 2023/06/13.
+//
+
+import SwiftUI
+
+struct ClearableTextField: View {
+    @Binding var text: String
+    @FocusState var isFocused: Bool
+
+    var placeholder: String
+
+    var body: some View {
+        HStack(spacing: 8) {
+            TextField(placeholder, text: $text)
+                .focused($isFocused)
+
+            if !text.isEmpty, isFocused {
+                Button {
+                    text = ""
+                } label: {
+                    Image(systemName: "multiply.circle.fill")
+                        .foregroundColor(.gray500)
+                }
+            }
+        }
+    }
+}
+
+struct ClearableTextFieldView: View {
+    @State private var text = ""
+    @FocusState private var isFocused: Bool
+
+    var body: some View {
+        ClearableTextField(text: $text, isFocused: _isFocused, placeholder: "Placeholder")
+    }
+}
+
+struct ClearableTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        ClearableTextFieldView()
+    }
+}
