29e028b08e231c6f3e12d3818fbcc5c4cbe5e404
zzimss <didwlsah9@gmail.com>
2023-06-27 21:53:06 +0900

    Add BackgroundViewModifier

---
+++ Utils/Utils/Views/ViewModifiers/BackgroundViewModifier.swift
@@ -0,0 +1,24 @@
+//
+//  BackgroundViewModifier.swift
+//  Utils
+//
+//  Created by Jmy on 2023/06/27.
+//
+
+import SwiftUI
+
+struct BackgroundViewModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        ZStack {
+            Color.gray800.ignoresSafeArea()
+
+            content
+        }
+    }
+}
+
+extension View {
+    func backgroundViewModifier() -> some View {
+        modifier(BackgroundViewModifier())
+    }
+}
