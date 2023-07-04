35b7ffcf61b0d21c6df851d8a07c04dae17add32
zzimss <didwlsah9@gmail.com>
2023-07-04 16:29:50 +0900

    Add badge

--- Gmail/Gmail/View/CustomTabBar.swift
+++ Gmail/Gmail/View/CustomTabBar.swift
@@ -14,12 +14,12 @@ struct CustomTabBar: View {
     var body: some View {
         HStack(spacing: 0) {
             ForEach(["envelope.fill", "video.fill"], id: \.self) { image in
-                TabButton(image: image, currentTab: $currentTab)
+                TabButton(image: image, currentTab: $currentTab, badge: image == "envelope.fill" ? 99 : 0)
             }
         }
         .padding(.top, 15)
         .padding(.bottom, bottomEdge)
-        .background(.white)
+        .background(Color("TabBG"))
     }
 }
 
@@ -33,6 +33,10 @@ struct TabButton: View {
     var image: String
     @Binding var currentTab: String
 
+    var badge: Int = 0
+
+    @Environment(\.colorScheme) var scheme
+
     var body: some View {
         Button {
             withAnimation {
@@ -45,6 +49,22 @@ struct TabButton: View {
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 35, height: 35)
                 .foregroundColor(currentTab == image ? Color("Pink") : Color.gray.opacity(0.7))
+                .overlay(
+                    Text("\(badge < 100 ? badge : 99)+")
+                        .font(.caption.bold())
+                        .foregroundColor(scheme == .dark ? .black : .white)
+                        .padding(.vertical, 4)
+                        .padding(.horizontal, 5)
+                        .background(Color("Pink"), in: Capsule())
+                        .background(
+                            Capsule()
+                                .stroke(scheme == .dark ? .black : .white, lineWidth: 4)
+                        )
+                        .offset(x: 20, y: -5)
+                        .opacity(badge == 0 ? 0 : 1)
+
+                    , alignment: .topTrailing
+                )
                 .frame(maxWidth: .infinity)
         }
     }
