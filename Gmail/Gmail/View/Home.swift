d2767f3fd8b42425b52d9bce11663ed3fb449d1b
zzimss <didwlsah9@gmail.com>
2023-07-04 16:42:03 +0900

    Add Floating Button

--- Gmail/Gmail/View/Home.swift
+++ Gmail/Gmail/View/Home.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct Home: View {
     @State var currentTab = "Mail"
-    
+
     var bottomEdge: CGFloat
 
     init(bottomEdge: CGFloat) {
@@ -17,9 +17,11 @@ struct Home: View {
         self.bottomEdge = bottomEdge
     }
 
+    @State var hideBar = false
+
     var body: some View {
         TabView(selection: $currentTab) {
-            Text("Mail")
+            MailView()
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .background(Color.primary.opacity(0.05))
                 .tag("Mail")
@@ -30,7 +32,30 @@ struct Home: View {
                 .tag("Meet")
         }
         .overlay(
-            CustomTabBar(currentTab: $currentTab, bottomEdge: bottomEdge),
+            VStack {
+                Button {
+                } label: {
+                    HStack(spacing: 10) {
+                        Image(systemName: "pencil")
+                            .font(.title)
+
+                        Text("Compose")
+                            .fontWeight(.semibold)
+                    }
+                    .foregroundColor(Color("Pink"))
+                    .padding(.vertical)
+                    .padding(.horizontal)
+                    .background(Color("TabBG"), in: Capsule())
+                    .shadow(color: .primary.opacity(0.06), radius: 5, x: 5, y: 10)
+                }
+                .padding(.trailing)
+                .offset(y: -15)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .opacity(currentTab == "envelope.fill" ? 1 : 0)
+                .animation(.none, value: currentTab)
+
+                CustomTabBar(currentTab: $currentTab, bottomEdge: bottomEdge)
+            },
             alignment: .bottom
         )
     }
---
+++ Gmail/Gmail/View/MailView.swift
@@ -0,0 +1,20 @@
+//
+//  MailView.swift
+//  Gmail
+//
+//  Created by Jmy on 2023/07/04.
+//
+
+import SwiftUI
+
+struct MailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MailView_Previews: PreviewProvider {
+    static var previews: some View {
+        MailView()
+    }
+}
