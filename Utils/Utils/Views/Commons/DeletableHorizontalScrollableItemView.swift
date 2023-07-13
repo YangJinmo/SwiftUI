0946a650e9d97430ccb8a8364f0d81467db5f083
zzimss <didwlsah9@gmail.com>
2023-07-13 18:40:34 +0900

    Add DeletableHorizontalScrollableItemView

---
+++ Utils/Utils/Views/Commons/DeletableHorizontalScrollableItemView.swift
@@ -0,0 +1,48 @@
+//
+//  DeletableHorizontalScrollableItemView.swift
+//  Utils
+//
+//  Created by Jmy on 2023/07/13.
+//
+
+import SwiftUI
+
+struct DeletableHorizontalScrollableItemView: View {
+    @Binding var texts: [String]
+    let textTapped: (String) -> Void
+    let xmarkTapped: (Int) -> Void
+
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 8) {
+                ForEach($texts, id: \.self) { text in
+                    DeletableItemView(text: text) { text in
+                        textTapped(text)
+                    } xmarkTapped: { text in
+                        if let index = texts.firstIndex(of: text) {
+                            xmarkTapped(index)
+                        }
+                    }
+                }
+            }
+            .padding(.horizontal, 16)
+        }
+    }
+}
+
+struct DeletableHorizontalScrollableItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        DeletableHorizontalScrollableItemView(
+            texts: Binding<[String]>.init(
+                get: { ["Apple", "Samsung"] },
+                set: { _ in }
+            ),
+            textTapped: { text in
+                print(text)
+            },
+            xmarkTapped: { index in
+                print(index)
+            }
+        )
+    }
+}
