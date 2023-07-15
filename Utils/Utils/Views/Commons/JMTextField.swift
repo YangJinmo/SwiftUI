a688299f63e2e77a2f072b377e316c2a8752ebfd
zzimss <didwlsah9@gmail.com>
2023-07-15 17:45:44 +0900

    Add JMTextField

---
+++ Utils/Utils/Views/Commons/JMTextField.swift
@@ -0,0 +1,53 @@
+//
+//  JMTextField.swift
+//  Utils
+//
+//  Created by Jmy on 2023/07/15.
+//
+
+import SwiftUI
+
+struct JMTextField: View {
+    @Binding var text: String
+    let placeholder: String
+
+    @FocusState private var isFocused: Bool
+
+    var body: some View {
+        TextField(
+            "",
+            text: $text,
+            prompt: promptText as? Text
+        )
+        .submitLabel(.next)
+        .font(.subheadline2)
+        .foregroundColor(.gray100)
+        .clearButton(text: $text, isFocused: _isFocused)
+        .padding(16)
+        .background(Color.gray700)
+        .cornerRadius(8)
+        .overlay(
+            RoundedRectangle(cornerRadius: 8)
+                .strokeBorder(
+                    isFocused
+                        ? Color.limeGreen
+                        : Color.clear,
+                    lineWidth: 1
+                )
+        )
+        .focused($isFocused)
+    }
+
+    var promptText: some View {
+        Text(placeholder)
+            .foregroundColor(.gray300)
+    }
+}
+
+struct HGTextField_Previews: PreviewProvider {
+    @State private static var productName = ""
+
+    static var previews: some View {
+        JMTextField(text: $productName, placeholder: "상품을 입력해 주세요.")
+    }
+}
