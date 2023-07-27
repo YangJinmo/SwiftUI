5cafcd219c6f761c0537f3b8216c2903e953dc41
zzimss <didwlsah9@gmail.com>
2023-07-28 02:13:52 +0900

    Add PullToRefresh

--- Utils/Utils/Views/BootCamps/Refreshable.swift
+++ Utils/Utils/Views/BootCamps/Refreshable.swift
@@ -46,3 +46,43 @@ struct Refreshable_Previews: PreviewProvider {
         Refreshable()
     }
 }
+
+struct PullToRefresh: View {
+    var coordinateSpaceName: String
+    var onRefresh: () -> Void
+
+    @State var needRefresh: Bool = false
+
+    var body: some View {
+        GeometryReader { geo in
+            if geo.frame(in: .named(coordinateSpaceName)).midY > 50 {
+                Spacer()
+                    .onAppear {
+                        needRefresh = true
+                    }
+
+            } else if geo.frame(in: .named(coordinateSpaceName)).maxY < 10 {
+                Spacer()
+                    .onAppear {
+                        if needRefresh {
+                            needRefresh = false
+                            onRefresh()
+                        }
+                    }
+            }
+
+            HStack {
+                Spacer()
+
+                if needRefresh {
+                    ProgressView()
+                } else {
+                    Text("⬇️")
+                }
+
+                Spacer()
+            }
+        }
+        .padding(.top, -50)
+    }
+}
