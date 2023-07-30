338b370188f9af3ac650f05b5791eeeaf538f054
zzimss <didwlsah9@gmail.com>
2023-07-31 00:04:04 +0900

    Add PullToRefreshView

--- Utils/Utils/Views/BootCamps/Refreshable.swift
+++ Utils/Utils/Views/BootCamps/Refreshable.swift
@@ -125,3 +125,67 @@ struct NewsItemView: View {
         .coordinateSpace(name: "pullToRefresh")
     }
 }
+
+import SwiftUI
+
+struct PullToRefreshView: View {
+    private static let minRefreshTimeInterval = TimeInterval(0.2)
+    private static let triggerHeight = CGFloat(100)
+    private static let indicatorHeight = CGFloat(100)
+    private static let fullHeight = triggerHeight + indicatorHeight
+
+    let backgroundColor: Color
+    let foregroundColor: Color
+    let isEnabled: Bool
+    let onRefresh: () -> Void
+
+    @State private var isRefreshIndicatorVisible = false
+    @State private var refreshStartTime: Date? = nil
+
+    init(bg: Color = .white, fg: Color = .black, isEnabled: Bool = true, onRefresh: @escaping () -> Void) {
+        backgroundColor = bg
+        foregroundColor = fg
+        self.isEnabled = isEnabled
+        self.onRefresh = onRefresh
+    }
+
+    var body: some View {
+        VStack(spacing: 0) {
+            LazyVStack(spacing: 0) {
+                Color.clear
+                    .frame(height: Self.triggerHeight)
+                    .onAppear {
+                        if isEnabled {
+                            withAnimation {
+                                isRefreshIndicatorVisible = true
+                            }
+                            refreshStartTime = Date()
+                        }
+                    }
+                    .onDisappear {
+                        if isEnabled, isRefreshIndicatorVisible, let diff = refreshStartTime?.distance(to: Date()), diff > Self.minRefreshTimeInterval {
+                            onRefresh()
+                        }
+                        withAnimation {
+                            isRefreshIndicatorVisible = false
+                        }
+                        refreshStartTime = nil
+                    }
+            }
+            .frame(height: Self.triggerHeight)
+
+            indicator
+                .frame(height: Self.indicatorHeight)
+        }
+        .background(backgroundColor)
+        .ignoresSafeArea(edges: .all)
+        .frame(height: Self.fullHeight)
+        .padding(.top, -Self.fullHeight)
+    }
+
+    private var indicator: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: foregroundColor))
+            .opacity(isRefreshIndicatorVisible ? 1 : 0)
+    }
+}
