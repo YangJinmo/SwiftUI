84cbe8e558233e788f2a2392175b03572ebf8982
zzimss <didwlsah9@gmail.com>
2023-08-04 13:04:01 +0900

    Add Foundation, Bundle

---
+++ Utils/Utils/Extensions/Foundation/Bundle.swift
@@ -0,0 +1,34 @@
+//
+//  Bundle.swift
+//  Utils
+//
+//  Created by Jmy on 2023/08/04.
+//
+
+import Foundation.NSBundle
+
+extension Bundle {
+    static var appName: String {
+        return main.infoDictionary?["CFBundleName"] as? String ?? "Unknown"
+    }
+
+    static var appDisplayName: String {
+        return main.infoDictionary?["CFBundleDisplayName"] as? String ?? appName
+    }
+
+    static var appVersion: String {
+        return main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "Unknown"
+    }
+
+    static var appBuild: String {
+        return main.infoDictionary?["CFBundleVersion"] as? String ?? "Unknown"
+    }
+
+    static var identifier: String {
+        return main.infoDictionary?["CFBundleIdentifier"] as? String ?? "Unknown"
+    }
+
+    static var appVersionAndBuild: String {
+        return "v \(appVersion) (\(appBuild))"
+    }
+}
