346e934afd357cf400c9c88e56e0ba2ae6af0b91
zzimss <didwlsah9@gmail.com>
2023-08-10 10:01:55 +0900

    Add SizePreferenceKey

---
+++ Utils/Utils/PreferenceKeys/SizePreferenceKey.swift
@@ -0,0 +1,42 @@
+//
+//  SizePreferenceKey.swift
+//  Utils
+//
+//  Created by Jmy on 2023/08/08.
+//
+
+import SwiftUI
+
+struct SizePreferenceKey: PreferenceKey {
+    static var defaultValue: CGSize = .zero
+
+    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {}
+}
+
+extension View {
+    // .readSize { size in totalHeight = size.height }
+    func readSize(onChange: @escaping (CGSize) -> Void) -> some View {
+        background(
+            GeometryReader { geometryProxy in
+                Color.clear
+                    .preference(key: SizePreferenceKey.self, value: geometryProxy.size)
+            }
+        )
+        .onPreferenceChange(SizePreferenceKey.self, perform: onChange)
+    }
+
+    // .readSize($totalSize)
+    func readSize(_ binding: Binding<CGSize>) -> some View {
+        background(
+            GeometryReader { geometry -> Color in
+                let rect = geometry.frame(in: .local)
+
+                DispatchQueue.main.async {
+                    binding.wrappedValue = rect.size
+                }
+
+                return .clear
+            }
+        )
+    }
+}
