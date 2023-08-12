3725aa3fa8af573d6878f2e481dc7eee689470a4
zzimss <didwlsah9@gmail.com>
2023-08-12 21:08:34 +0900

    Add RoundedRectangleToggleStyle

---
+++ Utils/Utils/Styles/ToggleStyles/RoundedRectangleToggleStyle.swift
@@ -0,0 +1,43 @@
+//
+//  RoundedRectangleToggleStyle.swift
+//  Utils
+//
+//  Created by Jmy on 2023/08/12.
+//
+
+import SwiftUI
+
+struct RoundedRectangleToggleStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        Button {
+            configuration.isOn.toggle()
+        } label: {
+            configuration.label
+                .font(.subheadline)
+                .foregroundColor(configuration.isOn ? .black : .gray100)
+                .padding(.horizontal, 16)
+                .padding(.vertical, 8)
+                .background(configuration.isOn ? .accentColor : Color.gray700)
+                .cornerRadius(8)
+        }
+    }
+}
+
+extension ToggleStyle where Self == RoundedRectangleToggleStyle {
+    static var roundedRectangle: RoundedRectangleToggleStyle { .init() }
+}
+
+struct RoundedRectangleToggleStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        RoundedRectangleToggleStylePreview()
+    }
+}
+
+struct RoundedRectangleToggleStylePreview: View {
+    @State private var isOn: Bool = false
+
+    var body: some View {
+        Toggle("RoundedRectangleToggleStyle", isOn: $isOn)
+            .toggleStyle(.roundedRectangle)
+    }
+}
