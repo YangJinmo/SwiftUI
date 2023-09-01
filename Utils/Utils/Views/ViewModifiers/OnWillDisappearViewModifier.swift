75ca951ce373b7c0073af428e8304fd163a9895c
zzimss <didwlsah9@gmail.com>
2023-09-01 11:31:32 +0900

    Add onWillDisappear

---
+++ Utils/Utils/Views/ViewModifiers/OnWillDisappearViewModifier.swift
@@ -0,0 +1,60 @@
+//
+//  OnWillDisappearViewModifier.swift
+//  Utils
+//
+//  Created by Jmy on 2023/09/01.
+//
+
+import SwiftUI
+
+struct WillDisappearHandler: UIViewControllerRepresentable {
+    func makeCoordinator() -> WillDisappearHandler.Coordinator {
+        Coordinator(onWillDisappear: onWillDisappear)
+    }
+
+    let onWillDisappear: () -> Void
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<WillDisappearHandler>) -> UIViewController {
+        context.coordinator
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<WillDisappearHandler>) {
+    }
+
+    typealias UIViewControllerType = UIViewController
+
+    class Coordinator: UIViewController {
+        let onWillDisappear: () -> Void
+
+        init(onWillDisappear: @escaping () -> Void) {
+            self.onWillDisappear = onWillDisappear
+
+            super.init(nibName: nil, bundle: nil)
+        }
+
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+
+        override func viewWillDisappear(_ animated: Bool) {
+            super.viewWillDisappear(animated)
+
+            onWillDisappear()
+        }
+    }
+}
+
+struct OnWillDisappearViewModifier: ViewModifier {
+    let callback: () -> Void
+
+    func body(content: Content) -> some View {
+        content
+            .background(WillDisappearHandler(onWillDisappear: callback))
+    }
+}
+
+extension View {
+    func onWillDisappear(_ perform: @escaping () -> Void) -> some View {
+        modifier(OnWillDisappearViewModifier(callback: perform))
+    }
+}
