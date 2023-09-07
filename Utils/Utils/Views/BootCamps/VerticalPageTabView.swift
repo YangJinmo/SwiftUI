fd1d8fc0feb7814adb9017b35c19a47051c77bcf
zzimss <didwlsah9@gmail.com>
2023-09-07 10:36:48 +0900

    Add VerticalPageTabView

---
+++ Utils/Utils/Views/BootCamps/VerticalPageTabView.swift
@@ -0,0 +1,33 @@
+//
+//  VerticalPageTabView.swift
+//  Utils
+//
+//  Created by Jmy on 2023/09/05.
+//
+
+import SwiftUI
+
+struct VerticalPageTabView: View {
+    var body: some View {
+        TabView {
+            Text("First")
+                .navigationTitle("First Title")
+
+            Text("Second")
+                .navigationTitle("Second Title")
+
+            List(1 ..< 50) { i in
+                Text("Row \(i)")
+            }
+            .navigationTitle("Third Title")
+        }
+        // .tabViewStyle(.verticalPage)
+        // Xcode 15 beta 1
+    }
+}
+
+struct VerticalPageTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        VerticalPageTabView()
+    }
+}
