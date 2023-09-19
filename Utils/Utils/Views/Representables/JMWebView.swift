9decb2ee4455041ff4a6590badfa6d31b4bd7d39
zzimss <didwlsah9@gmail.com>
2023-09-20 01:42:44 +0900

    Update HTMLWebView to JMWebView

---
+++ Utils/Utils/Views/Representables/JMWebView.swift
@@ -0,0 +1,128 @@
+//
+//  JMWebView.swift
+//  Utils
+//
+//  Created by Jmy on 2023/06/05.
+//
+
+import SwiftUI
+import WebKit
+
+final class JMWebView: WKWebView {
+    override init(frame: CGRect, configuration: WKWebViewConfiguration) {
+        super.init(frame: frame, configuration: configuration)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    init(url: URL) {
+        let configuration = WKWebViewConfiguration()
+        super.init(frame: .zero, configuration: configuration)
+
+        isOpaque = false
+
+        let urlRequest = URLRequest(url: url)
+        load(urlRequest)
+    }
+
+    init(htmlString: String) {
+        let configuration = WKWebViewConfiguration()
+        super.init(frame: .zero, configuration: configuration)
+
+        isOpaque = false
+
+        loadHTMLString(htmlString, baseURL: nil)
+    }
+}
+
+struct HTMLWebNavigationView: View {
+    @Binding var isActiving: Bool
+
+    let title: String
+    let html: String
+
+    var body: some View {
+        JMView(.navigation, $isActiving) {
+            Text(title)
+        } content: {
+            ViewRepresentable {
+                JMWebView(htmlString: html)
+            }
+        }
+        .edgesIgnoringSafeArea(.bottom)
+    }
+}
+
+struct HTMLWebNavigationView_Previews: PreviewProvider {
+    static var previews: some View {
+        JMView {
+            Text("https://www.naver.com")
+        } content: {
+            ViewRepresentable {
+                JMWebView(url: "https://www.naver.com".toURL!)
+            }
+        }
+        .edgesIgnoringSafeArea(.bottom)
+
+        JMView {
+            Text("Hello, world!")
+        } content: {
+            ViewRepresentable {
+                JMWebView(htmlString: """
+                    <html>
+                    <body>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my website</p>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my website</p>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my website</p>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my website</p>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my website</p>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my website</p>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my website</p>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my website</p>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my website</p>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my website</p>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my website</p>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my website</p>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my website</p>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my website</p>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my website</p>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my website</p>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my website</p>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my website</p>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my website</p>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my website</p>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my website</p>
+                    <h1>Hello, world!</h1>
+                    <p>Welcome to my website</p>
+                    </body>
+                    </html>
+                    """
+                )
+            }
+        }
+        .edgesIgnoringSafeArea(.bottom)
+    }
+}
