177af179134db11332d33b223b7f8d6e3908ad57
zzimss <didwlsah9@gmail.com>
2023-10-01 12:31:56 +0900

    Add AnalogClock

---
+++ Utils/Utils/Views/BootCamps/AnalogClock.swift
@@ -0,0 +1,18 @@
+//
+//  AnalogClock.swift
+//  Utils
+//
+//  Created by Jmy on 2023/10/01.
+//
+
+import SwiftUI
+
+struct AnalogClock: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnalogClock()
+}
