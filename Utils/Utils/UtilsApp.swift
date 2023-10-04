01e5a221e02def4af43b5b1ea03ba32ce6f89f17
zzimss <didwlsah9@gmail.com>
2023-10-04 22:22:01 +0900

    Add Clock

--- Utils/Utils/UtilsApp.swift
+++ Utils/Utils/UtilsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct UtilsApp: App {
     var body: some Scene {
         WindowGroup {
-            CollectionViewPreview()
+            AnalogClock()
         }
     }
 }
--- Utils/Utils/Views/BootCamps/AnalogClock.swift
+++ Utils/Utils/Views/BootCamps/AnalogClock.swift
@@ -11,8 +11,20 @@ struct AnalogClock: View {
     @ObservedObject var time = CurrentTime()
 
     var body: some View {
-        return ZStack {
-            Text("\(time.seconds)")
+        ZStack {
+            // Text("\(time.seconds)")
+
+            Clock(model: .init(type: .hour, timeInterval: time.seconds, tickScale: 0.4))
+                .stroke(Color.primary, style: StrokeStyle(lineWidth: 4, lineCap: .round, lineJoin: .round))
+                .rotationEffect(Angle.degrees(360 / 60))
+
+            Clock(model: .init(type: .minute, timeInterval: time.seconds, tickScale: 0.6))
+                .stroke(Color.blue, style: StrokeStyle(lineWidth: 3, lineCap: .round, lineJoin: .round))
+                .rotationEffect(Angle.degrees(360 / 60))
+
+            Clock(model: .init(type: .second, timeInterval: time.seconds, tickScale: 0.8))
+                .stroke(Color.red, style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round))
+                .rotationEffect(Angle.degrees(360 / 60))
         }
         .frame(width: 200, height: 200, alignment: .center)
     }
@@ -39,5 +51,55 @@ final class CurrentTime: ObservableObject {
         let components = Calendar.current.dateComponents([.year, .month, .day], from: date)
         let referenceDate = Calendar.current.date(from: DateComponents(year: components.year!, month: components.month!, day: components.day!))!
         return Date().timeIntervalSince(referenceDate)
+    }
+}
+
+struct Clock: Shape {
+    var model: ClockTickerModel
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let length = rect.width / 2
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+
+        path.move(to: center)
+        let hoursAngle = CGFloat.pi / 2 - .pi * 2 * model.angleMultiplier
+        path.addLine(to: CGPoint(x: rect.midX + cos(hoursAngle) * length * model.tickerScale,
+                                 y: rect.midY - sin(hoursAngle) * length * model.tickerScale))
+        return path
+    }
+}
+
+struct ClockTickerModel {
+    enum TickerType {
+        case second
+        case hour
+        case minute
+    }
+
+    let type: TickerType
+    let timeInterval: TimeInterval
+    let tickScale: CGFloat
+
+    var angleMultiplier: CGFloat {
+        switch type {
+        case .second:
+            return CGFloat(timeInterval.remainder(dividingBy: 60)) / 60
+        case .hour:
+            return CGFloat(timeInterval / 3600) / 12
+        case .minute:
+            return CGFloat((timeInterval - Double(Int(timeInterval / 3600) * 3600)) / 60) / 60
+        }
+    }
+
+    var tickerScale: CGFloat {
+        switch type {
+        case .second:
+            return 0.8
+        case .hour:
+            return 0.4
+        case .minute:
+            return 0.6
+        }
     }
 }
