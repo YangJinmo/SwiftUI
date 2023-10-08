a739fa312284179a8fda76c941f4ebfbb91dcaef
zzimss <didwlsah9@gmail.com>
2023-10-08 19:43:09 +0900

    Move FitToAspectRatio

--- Utils/Utils/Views/BootCamps/CollectionViewBootCamp.swift
+++ Utils/Utils/Views/BootCamps/CollectionViewBootCamp.swift
@@ -212,64 +212,7 @@ struct Card: View {
     }
 }
 
-// https://gist.github.com/karigrooms/fdf435274f4403abd57b1ed533dcea53
 
-import SwiftUI
-
-/// Common aspect ratios
-public enum AspectRatio: CGFloat {
-    case square = 1
-    case threeToFour = 0.75
-    case fourToThree = 1.75
-}
-
-/// Fit an image to a certain aspect ratio while maintaining its aspect ratio
-public struct FitToAspectRatio: ViewModifier {
-    private let aspectRatio: CGFloat
-
-    public init(_ aspectRatio: CGFloat) {
-        self.aspectRatio = aspectRatio
-    }
-
-    public init(_ aspectRatio: AspectRatio) {
-        self.aspectRatio = aspectRatio.rawValue
-    }
-
-    public func body(content: Content) -> some View {
-        ZStack {
-            Rectangle()
-                .fill(Color(.clear))
-                .aspectRatio(aspectRatio, contentMode: .fit)
-
-            content
-                .scaledToFill()
-                .layoutPriority(-1)
-        }
-        .clipped()
-    }
-}
-
-// Image extension that composes with the `.resizable()` modifier
-public extension Image {
-    func fitToAspectRatio(_ aspectRatio: CGFloat) -> some View {
-        resizable().modifier(FitToAspectRatio(aspectRatio))
-    }
-
-    func fitToAspectRatio(_ aspectRatio: AspectRatio) -> some View {
-        resizable().modifier(FitToAspectRatio(aspectRatio))
-    }
-}
-
-// Image extension that composes with the `.resizable()` modifier
-extension AsyncImageView {
-    func fitToAspectRatio(_ aspectRatio: CGFloat) -> some View {
-        modifier(FitToAspectRatio(aspectRatio))
-    }
-
-    func fitToAspectRatio(_ aspectRatio: AspectRatio) -> some View {
-        modifier(FitToAspectRatio(aspectRatio))
-    }
-}
 
 extension UIImageView {
     // MARK: - Error
