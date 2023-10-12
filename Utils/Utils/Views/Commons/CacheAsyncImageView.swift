55071e5a862b91b928da1de22a581edc08c7e451
zzimss <didwlsah9@gmail.com>
2023-10-12 19:00:51 +0900

    Add CacheAsyncImageView

---
+++ Utils/Utils/Views/Commons/CacheAsyncImageView.swift
@@ -0,0 +1,41 @@
+//
+//  CacheAsyncImageView.swift
+//  Utils
+//
+//  Created by Jmy on 2023/10/12.
+//
+
+import SwiftUI
+
+struct CacheAsyncImageView: View {
+    var url: String?
+
+    var body: some View {
+        if let url = url?.toURL {
+            CacheAsyncImage(url: url) { phase in
+                switch phase {
+                case .empty:
+                    ProgressView()
+
+                case let .success(image):
+                    image.resizable()
+                        .scaledToFit()
+
+                case .failure:
+                    Image.photo
+                        .foregroundColor(.primary)
+
+                @unknown default:
+                    EmptyView()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    CacheAsyncImageView(url: "https://www.forbes.com/advisor/wp-content/uploads/2022/10/condo-vs-apartment.jpeg.jpg")
+        .fitToAspectRatio(3 / 2)
+        .clipShape(RoundedRectangle(cornerRadius: 4))
+        .padding()
+}
