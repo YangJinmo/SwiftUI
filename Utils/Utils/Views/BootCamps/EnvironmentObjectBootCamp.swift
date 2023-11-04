379e5c98d144ae6457beab71c55d408a767d2958
zzimss <didwlsah9@gmail.com>
2023-11-04 14:31:46 +0900

    Add EnvironmentObjectBootCamp

---
+++ Utils/Utils/Views/BootCamps/EnvironmentObjectBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  EnvironmentObjectBootCamp.swift
+//  Utils
+//
+//  Created by Jmy on 2023/11/04.
+//
+
+import SwiftUI
+
+struct EnvironmentObjectBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EnvironmentObjectBootCamp()
+}
