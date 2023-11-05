c96d50b834843a517d654f949396c8c42cbe4f12
zzimss <didwlsah9@gmail.com>
2023-11-05 12:53:29 +0900

    Add EnvironmentObjectBootCamp

--- Utils/Utils/Views/BootCamps/EnvironmentObjectBootCamp.swift
+++ Utils/Utils/Views/BootCamps/EnvironmentObjectBootCamp.swift
@@ -7,12 +7,87 @@
 
 import SwiftUI
 
-struct EnvironmentObjectBootCamp: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class EnvironmentViewModel: ObservableObject {
+    @Published var dataArray: [String] = []
+
+    init() {
+        getData()
+    }
+
+    func getData() {
+        dataArray.append(contentsOf: ["iPhone", "iPad", "iMac", "Apple Watch"])
     }
 }
 
-#Preview {
-    EnvironmentObjectBootCamp()
+struct EnvironmentObjectBootCamp: View {
+    @StateObject var viewModel: EnvironmentViewModel = EnvironmentViewModel()
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(viewModel.dataArray, id: \.self) { item in
+                    NavigationLink {
+                        DetailView(selectedItem: item)
+                    } label: {
+                        Text(item)
+                    }
+                }
+            }
+            .navigationTitle("iOS Devices")
+        }
+        .environmentObject(viewModel)
+    }
+}
+
+struct DetailView: View {
+    let selectedItem: String
+
+    var body: some View {
+        ZStack {
+            Color.orange.ignoresSafeArea()
+
+            NavigationLink {
+                FinalView()
+            } label: {
+                Text(selectedItem)
+                    .font(.headline)
+                    .foregroundColor(.orange)
+                    .padding()
+                    .padding(.horizontal)
+                    .background(Color.white)
+                    .cornerRadius(30)
+            }
+        }
+    }
+}
+
+struct FinalView: View {
+    @EnvironmentObject var viewModel: EnvironmentViewModel
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [Color.red, Color.blue]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .ignoresSafeArea()
+
+            ScrollView {
+                VStack(spacing: 20) {
+                    ForEach(viewModel.dataArray, id: \.self) { item in
+                        Text(item)
+                    }
+                }
+                .foregroundColor(.white)
+                .font(.largeTitle)
+            }
+        }
+    }
+}
+
+struct EnvironmentObjectBootCamp_Previews: PreviewProvider {
+    static var previews: some View {
+        EnvironmentObjectBootCamp()
+    }
 }
