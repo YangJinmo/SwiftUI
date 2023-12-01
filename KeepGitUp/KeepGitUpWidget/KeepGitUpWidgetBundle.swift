a7c67c92c73343ccc5fe7b7127eba1d5ef361c83
zzimss <didwlsah9@gmail.com>
2023-12-01 18:39:32 +0900

    Add New Target KeepGitUpWidget of type Intent

---
+++ KeepGitUp/KeepGitUpWidget/KeepGitUpWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  KeepGitUpWidgetBundle.swift
+//  KeepGitUpWidget
+//
+//  Created by Jmy on 2023/12/01.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct KeepGitUpWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        KeepGitUpWidget()
+        KeepGitUpWidgetLiveActivity()
+    }
+}
