567cb7cc8793124d5271bec1f02ce0dd37c9b349
zzimss <didwlsah9@gmail.com>
2023-12-04 23:54:12 +0900

    Add SampleKeyboard.project, KeyboardViewController.swift

---
+++ SampleKeyboard/SampleKeyboard/SampleKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleKeyboardApp.swift
+//  SampleKeyboard
+//
+//  Created by Jmy on 2023/12/04.
+//
+
+import SwiftUI
+
+@main
+struct SampleKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
