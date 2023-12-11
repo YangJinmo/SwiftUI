f273d51aba3399cebec1bc05a29943ca2ce2a197
zzimss <didwlsah9@gmail.com>
2023-12-11 12:00:35 +0900

    Add app life-cycle

--- Utils/Utils/UtilsApp.swift
+++ Utils/Utils/UtilsApp.swift
@@ -9,9 +9,28 @@ import SwiftUI
 
 @main
 struct UtilsApp: App {
+    @Environment(\.scenePhase) var phase
+
     var body: some Scene {
         WindowGroup {
             AnalogClock()
+                .onAppear {
+                    "onAppear".log(trait: .app)
+                }
+                .onDisappear {
+                    "onDisappear".log(trait: .app)
+                }
         }
+        .onChange(of: phase) { newValue in
+            if newValue == .active {
+                "active".log(trait: .app)
+            } else if newValue == .inactive {
+                "inactive".log(trait: .app)
+            } else if newValue == .background {
+                "background".log(trait: .app)
+            }
+        }
+        // inactive 상황에서 앱을 메모리에서 날리면 background
+        // background 상황에서 앱을 메모리에서 날리면 onDisappear
     }
 }
