e2b76ab496c172271aabc8b6be44355fc19aefab
zzimss <didwlsah9@gmail.com>
2023-12-12 16:49:15 +0900

    Add comment

--- Utils/Utils/UtilsApp.swift
+++ Utils/Utils/UtilsApp.swift
@@ -30,7 +30,21 @@ struct UtilsApp: App {
                 "background".log(trait: .app)
             }
         }
-        // inactive 상황에서 앱을 메모리에서 날리면 background
-        // background 상황에서 앱을 메모리에서 날리면 onDisappear
+
+        // MARK: Scene Phase
+
+        // 앱 사용 시간
+        // 유저가 앱을 시작한 시간부터 앱을 사용하지 않는 시간을 찾으면 우리는 유저가 앱을 사용한 시간을 찾을 수 있다.
+        // inactive는 active에서 background로 이동 시, background에서 active로 이동 시 두 경우 모두 호출되기 때문에
+        // 우리에게 필요한 앱 사용 종료를 의미하는 값은 active에서 inactive가 될 경우이다.
+        // 앱 사용 시작: active
+        // 앱 사용 종료: active -> inactive (이전 상태가 active일 때, inactive가 호출된 시점)
+        // 현재 scenePhase를 저장하고 inactive 호출 시, 만약 저장된 scenePhase가 active일 경우를ㄱㄱㄹ ㅍㅍ
+        // 유저의 앱 사용 종료로 볼 수 있다.
+
+        // 앱 메모리 해제
+        // background 상태에서 앱을 메모리에서 날리면 onDisappear가 정상적으로 불리고
+        // inactive 상태에서 앱을 메모리에서 날리면 background까지는 불리지만 onDisappear까지는 불리지 않는다.
+        // 따라서, 첫번째 경우를 제외하면, 앱이 메모리에서 날아가는 상황을 감지하기 위해서는 추가 작업이 필요할 것으로 보인다.
     }
 }
