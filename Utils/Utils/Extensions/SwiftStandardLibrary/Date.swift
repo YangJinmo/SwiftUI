67f4d17f2d1a6128ac342e8878f898ba7052d705
zzimss <didwlsah9@gmail.com>
2023-12-18 23:53:30 +0900

    Update timeZone, Add locale

--- Utils/Utils/Extensions/SwiftStandardLibrary/Date.swift
+++ Utils/Utils/Extensions/SwiftStandardLibrary/Date.swift
@@ -10,8 +10,8 @@ import Foundation.NSDate
 extension Date {
     func toString(dateFormat: String = "yyyy.MM.dd HH:mm:ss:SSS") -> String {
         let dateFormatter = DateFormatter()
-        // dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
-        dateFormatter.timeZone = .current
+        dateFormatter.timeZone = .current // TimeZone(abbreviation: "GMT")
+        dateFormatter.locale = .current
         dateFormatter.dateFormat = dateFormat
         return dateFormatter.string(from: self)
     }
