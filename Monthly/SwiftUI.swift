50e739c4e89947fe1b30fc97e23689c2b1ea25a5
zzimss <didwlsah9@gmail.com>
2024-01-10 22:41:29 +0900

    Add determineConfig of MonthConfig

---
+++ Monthly/SwiftUI.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftUI.swift
+//  MonthlyWidgetExtension
+//
+//  Created by Jmy on 1/10/24.
+//
+
+import SwiftUI
+
+struct MonthConfig {
+    let backgroundColor: Color
+    let emojiText: String
+    let weekdayTextColor: Color
+    let dayTextColor: Color
+
+    static func determineConfig(from date: Date) -> MonthConfig {
+        let monthInt = Calendar.current.component(.month, from: date)
+
+        switch monthInt {
+        case 1:
+            return MonthConfig(
+                backgroundColor: .gray,
+                emojiText: "💂",
+                weekdayTextColor: .black.opacity(0.6),
+                dayTextColor: .white.opacity(0.8)
+            )
+        default:
+            return MonthConfig(
+                backgroundColor: .gray,
+                emojiText: "💂",
+                weekdayTextColor: .black.opacity(0.6),
+                dayTextColor: .white.opacity(0.8)
+            )
+        }
+    }
+}
