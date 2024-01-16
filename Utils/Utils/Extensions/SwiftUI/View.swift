adf0a8171159e8f7262b71821b0e521b0694e7a0
zzimss <didwlsah9@gmail.com>
2024-01-16 15:04:52 +0900

    Add if function

--- Utils/Utils/Extensions/SwiftUI/View.swift
+++ Utils/Utils/Extensions/SwiftUI/View.swift
@@ -37,4 +37,12 @@ extension View {
             self
         }
     }
+
+    @ViewBuilder func `if`<Transform: View>(_ condition: Bool, transform: (Self) -> Transform) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
 }
