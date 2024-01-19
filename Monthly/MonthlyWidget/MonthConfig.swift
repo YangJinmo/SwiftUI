042d3843962c15a743a3dbdfbee0efc6c5dbaff3
zzimss <didwlsah9@gmail.com>
2024-01-19 13:53:44 +0900

    Add MonthConfig

--- Monthly/MonthlyWidget/MonthConfig.swift
+++ Monthly/MonthlyWidget/MonthConfig.swift
@@ -24,12 +24,89 @@ struct MonthConfig {
                 weekdayTextColor: .black.opacity(0.6),
                 dayTextColor: .white.opacity(0.8)
             )
-        default:
+        case 2:
             return MonthConfig(
                 backgroundColor: .palePink,
                 emojiText: "❤️",
                 weekdayTextColor: .black.opacity(0.5),
                 dayTextColor: .pink.opacity(0.8)
+            )
+        case 3:
+            return MonthConfig(
+                backgroundColor: .paleGreen,
+                emojiText: "☘️",
+                weekdayTextColor: .black.opacity(0.7),
+                dayTextColor: .darkGreen.opacity(0.8)
+            )
+        case 4:
+            return MonthConfig(
+                backgroundColor: .paleBlue,
+                emojiText: "☔️",
+                weekdayTextColor: .black.opacity(0.5),
+                dayTextColor: .purple.opacity(0.8)
+            )
+        case 5:
+            return MonthConfig(
+                backgroundColor: .paleYellow,
+                emojiText: "🌺",
+                weekdayTextColor: .black.opacity(0.6),
+                dayTextColor: .pink.opacity(0.8)
+            )
+        case 6:
+            return MonthConfig(
+                backgroundColor: .skyBlue,
+                emojiText: "🌤️",
+                weekdayTextColor: .black.opacity(0.5),
+                dayTextColor: .paleYellow.opacity(0.8)
+            )
+        case 7:
+            return MonthConfig(
+                backgroundColor: .blue,
+                emojiText: "🏖️",
+                weekdayTextColor: .black.opacity(0.6),
+                dayTextColor: .paleBlue.opacity(0.8)
+            )
+        case 8:
+            return MonthConfig(
+                backgroundColor: .paleOrange,
+                emojiText: "☀️",
+                weekdayTextColor: .black.opacity(0.5),
+                dayTextColor: .darkOrange.opacity(0.8)
+            )
+        case 9:
+            return MonthConfig(
+                backgroundColor: .paleRed,
+                emojiText: "🍁",
+                weekdayTextColor: .black.opacity(0.6),
+                dayTextColor: .paleYellow.opacity(0.8)
+            )
+        case 10:
+            return MonthConfig(
+                backgroundColor: .black,
+                emojiText: "👻",
+                weekdayTextColor: .black.opacity(0.5),
+                dayTextColor: .orange.opacity(0.8)
+            )
+        case 11:
+            return MonthConfig(
+                backgroundColor: .paleBrown,
+                emojiText: "🦃",
+                weekdayTextColor: .black.opacity(0.6),
+                dayTextColor: .black.opacity(0.8)
+            )
+        case 12:
+            return MonthConfig(
+                backgroundColor: .paleRed,
+                emojiText: "🎄",
+                weekdayTextColor: .black.opacity(0.5),
+                dayTextColor: .darkGreen.opacity(0.8)
+            )
+        default:
+            return MonthConfig(
+                backgroundColor: .gray,
+                emojiText: "📅",
+                weekdayTextColor: .black.opacity(0.5),
+                dayTextColor: .white.opacity(0.8)
             )
         }
     }
