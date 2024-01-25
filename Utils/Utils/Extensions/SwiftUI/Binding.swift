fee4faf4047515d3806e539a6e52b95018745388
zzimss <didwlsah9@gmail.com>
2024-01-25 13:45:14 +0900

    Add init

--- Utils/Utils/Extensions/SwiftUI/Binding.swift
+++ Utils/Utils/Extensions/SwiftUI/Binding.swift
@@ -7,6 +7,18 @@
 
 import SwiftUI
 
+extension Binding where Value == Bool {
+    init<T>(value: Binding<T?>) {
+        self.init {
+            value.wrappedValue != nil
+        } set: { newValue in
+            if !newValue {
+                value.wrappedValue = nil
+            }
+        }
+    }
+}
+
 extension Binding where Value == String {
     func max(_ limit: Int) -> Self {
         if wrappedValue.count > limit {
