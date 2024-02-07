8a69237a42f7bac39ce86719c551cedfb596deb3
zzimss <didwlsah9@gmail.com>
2024-02-08 00:05:07 +0900

    Add DefaultButtonViewModifier

--- Utils/Utils/Views/BootCamps/ViewModifierBootcamp.swift
+++ Utils/Utils/Views/BootCamps/ViewModifierBootcamp.swift
@@ -7,18 +7,42 @@
 
 import SwiftUI
 
+struct DefaultButtonViewModifier: ViewModifier {
+    let backgroundColor: Color
+
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(.white)
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .background(backgroundColor)
+            .cornerRadius(10)
+            .shadow(radius: 10)
+    }
+}
+
+extension View {
+    func withDefaultButtonFommatting(backgroundColor: Color = .blue) -> some View {
+        modifier(DefaultButtonViewModifier(backgroundColor: backgroundColor))
+    }
+}
+
 struct ViewModifierBootcamp: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 10) {
             Text("Hello, world!")
                 .font(.headline)
-                .foregroundColor(.white)
-                .frame(height: 55)
-                .frame(maxWidth: .infinity)
-                .background(Color.blue)
-                .cornerRadius(10)
-                .shadow(radius: 10)
+                .withDefaultButtonFommatting()
+
+            Text("Hello, everyone!")
+                .font(.subheadline)
+                .withDefaultButtonFommatting(backgroundColor: .orange)
+
+            Text("Hello!!!")
+                .font(.title)
+                .withDefaultButtonFommatting(backgroundColor: .pink)
         }
+        .padding()
     }
 }
 
