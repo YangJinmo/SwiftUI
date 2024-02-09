4dc076e00f239f4e9d5784ee2a87eae67a5f6ff2
zzimss <didwlsah9@gmail.com>
2024-02-10 01:09:30 +0900

    Add AnyTransitionBootcamp

---
+++ Utils/Utils/Views/BootCamps/AnyTransitionBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  AnyTransitionBootcamp.swift
+//  Utils
+//
+//  Created by Jmy on 2/10/24.
+//
+
+import SwiftUI
+
+struct AnyTransitionBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnyTransitionBootcamp()
+}
