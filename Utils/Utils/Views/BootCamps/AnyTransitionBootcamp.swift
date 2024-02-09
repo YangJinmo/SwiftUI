9071160197d2f6c9ea897d2d7380fadfcd1cb99c
zzimss <didwlsah9@gmail.com>
2024-02-10 01:15:34 +0900

    Update AnyTransitionBootcamp

--- Utils/Utils/Views/BootCamps/AnyTransitionBootcamp.swift
+++ Utils/Utils/Views/BootCamps/AnyTransitionBootcamp.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct AnyTransitionBootcamp: View {
+    @State private var showRectangle: Bool = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+
+            if showRectangle {
+                RoundedRectangle(cornerRadius: 25)
+                    .frame(width: 250, height: 350)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    // .transition(.move(edge: .leading))
+                    .transition(AnyTransition.scale.animation(.easeInOut))
+            }
+
+            Spacer()
+
+            Text("Click Me!")
+                .withDefaultButtonFommatting()
+                .padding(40)
+                .onTapGesture {
+                    withAnimation {
+                        showRectangle.toggle()
+                    }
+                }
+        }
     }
 }
 
