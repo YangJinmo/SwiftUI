1bbcb305e65cf6118e1e7cfa733d15c9fff99dd2
zzimss <didwlsah9@gmail.com>
2024-02-11 19:29:39 +0900

    Add RotationViewModifier

--- Utils/Utils/Views/BootCamps/AnyTransitionBootcamp.swift
+++ Utils/Utils/Views/BootCamps/AnyTransitionBootcamp.swift
@@ -7,6 +7,42 @@
 
 import SwiftUI
 
+struct RotationViewModifier: ViewModifier {
+    let rotation: Double
+
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(Angle(degrees: rotation))
+            .offset(
+                x: rotation != 0 ? UIScreen.main.bounds.width : 0,
+                y: rotation != 0 ? UIScreen.main.bounds.height : 0
+            )
+    }
+}
+
+extension AnyTransition {
+    static var rotating: AnyTransition {
+        return .modifier(
+            active: RotationViewModifier(rotation: 180),
+            identity: RotationViewModifier(rotation: 0)
+        )
+    }
+
+    static func rotating(rotation: Double) -> AnyTransition {
+        return .modifier(
+            active: RotationViewModifier(rotation: rotation),
+            identity: RotationViewModifier(rotation: 0)
+        )
+    }
+
+    static var rotateOn: AnyTransition {
+        return .asymmetric(
+            insertion: .rotating,
+            removal: .move(edge: .leading)
+        )
+    }
+}
+
 struct AnyTransitionBootcamp: View {
     @State private var showRectangle: Bool = false
 
@@ -19,7 +55,10 @@ struct AnyTransitionBootcamp: View {
                     .frame(width: 250, height: 350)
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                     // .transition(.move(edge: .leading))
-                    .transition(AnyTransition.scale.animation(.easeInOut))
+                    // .transition(AnyTransition.scale.animation(.easeInOut))
+                    // .transition(AnyTransition.rotating.animation(.easeInOut))
+                    // .transition(.rotating(rotation: 1080))
+                    .transition(.rotateOn)
             }
 
             Spacer()
@@ -28,7 +67,7 @@ struct AnyTransitionBootcamp: View {
                 .withDefaultButtonFommatting()
                 .padding(40)
                 .onTapGesture {
-                    withAnimation {
+                    withAnimation(.easeInOut(duration: 0.5)) {
                         showRectangle.toggle()
                     }
                 }
