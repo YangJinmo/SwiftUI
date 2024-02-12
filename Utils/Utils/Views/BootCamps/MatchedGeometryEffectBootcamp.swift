cd9f9876b79cacc27f5495c4e279cc8d60bfe721
zzimss <didwlsah9@gmail.com>
2024-02-12 12:27:24 +0900

    Add isClicked

--- Utils/Utils/Views/BootCamps/MatchedGeometryEffectBootcamp.swift
+++ Utils/Utils/Views/BootCamps/MatchedGeometryEffectBootcamp.swift
@@ -8,12 +8,28 @@
 import SwiftUI
 
 struct MatchedGeometryEffectBootcamp: View {
+    @State private var isClicked: Bool = false
+
     var body: some View {
         VStack {
-            RoundedRectangle(cornerRadius: 25.0)
-                .frame(width: 100, height: 100)
+            if !isClicked {
+                RoundedRectangle(cornerRadius: 25.0)
+                    .frame(width: 100, height: 100)
+            }
 
             Spacer()
+
+            if isClicked {
+                RoundedRectangle(cornerRadius: 25.0)
+                    .frame(width: 100, height: 100)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.red)
+        .onTapGesture {
+            withAnimation(.easeInOut) {
+                isClicked.toggle()
+            }
         }
     }
 }
