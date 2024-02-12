0e4c17ccceac9a0d437e8d6e2dc78a734b27330b
zzimss <didwlsah9@gmail.com>
2024-02-12 11:53:02 +0900

    Add MatchedGeometryEffectBootcamp

---
+++ Utils/Utils/Views/BootCamps/MatchedGeometryEffectBootcamp.swift
@@ -0,0 +1,23 @@
+//
+//  MatchedGeometryEffectBootcamp.swift
+//  Utils
+//
+//  Created by Jmy on 2/12/24.
+//
+
+import SwiftUI
+
+struct MatchedGeometryEffectBootcamp: View {
+    var body: some View {
+        VStack {
+            RoundedRectangle(cornerRadius: 25.0)
+                .frame(width: 100, height: 100)
+
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    MatchedGeometryEffectBootcamp()
+}
