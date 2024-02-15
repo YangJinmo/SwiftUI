63f9ea7402521ce8dbe8ce6fc823deeab42a69f9
zzimss <didwlsah9@gmail.com>
2024-02-15 10:42:13 +0900

    Add RegularRhombus, Rhombus, Trapezoid

--- Utils/Utils/Views/BootCamps/CustomShapeBootcamp.swift
+++ Utils/Utils/Views/BootCamps/CustomShapeBootcamp.swift
@@ -18,6 +18,44 @@ struct Triangle: Shape {
     }
 }
 
+struct RegularRhombus: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        }
+    }
+}
+
+struct Rhombus: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            let horizontalOffset: CGFloat = rect.width * 0.2
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX - horizontalOffset, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        }
+    }
+}
+
+struct Trapezoid: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            let horizontalOffset: CGFloat = rect.width * 0.2
+            path.move(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX - horizontalOffset, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        }
+    }
+}
+
 struct CustomShapeBootcamp: View {
     var body: some View {
         ZStack {
@@ -25,10 +63,30 @@ struct CustomShapeBootcamp: View {
 //                .trim(from: 0, to: 0.5)
 //                .frame(width: 300, height: 300)
 
-            Triangle()
-                .stroke(style: StrokeStyle(lineWidth: 5, lineCap: .round, dash: [10]))
-                .foregroundColor(.blue)
+//            Triangle()
+//                .stroke(style: StrokeStyle(lineWidth: 5, lineCap: .round, dash: [10]))
+//                .foregroundColor(.blue)
+//                .frame(width: 300, height: 300)
+
+//            RegularRhombus()
+//                .frame(width: 300, height: 300)
+
+//            Rhombus()
+//                .frame(width: 300, height: 300)
+
+            Trapezoid()
+                .fill(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
                 .frame(width: 300, height: 300)
+
+//            Image("")
+//                .resizable()
+//                .scaledToFill()
+//                .frame(width: 300, height: 300)
+//                .clipShape(
+//                    // Circle()
+//                    Triangle()
+//                        .rotation(Angle(degrees: 180))
+//                )
         }
     }
 }
