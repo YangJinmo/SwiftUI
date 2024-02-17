1b0506ff3b99b54f4dcf2c5647f8843005ebcf82
zzimss <didwlsah9@gmail.com>
2024-02-17 22:09:49 +0900

    Add ShapeWithArc

--- Utils/Utils/Views/BootCamps/CustomCurvesBootcamp.swift
+++ Utils/Utils/Views/BootCamps/CustomCurvesBootcamp.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct CustomCurvesBootcamp: View {
     var body: some View {
-        ArcSample()
+//        ArcSample()
+        ShapeWithArc()
+            .stroke(lineWidth: 5)
             .frame(width: 200, height: 200)
     }
 }
@@ -25,10 +27,41 @@ struct ArcSample: Shape {
             path.addArc(
                 center: CGPoint(x: rect.midX, y: rect.midY),
                 radius: rect.height / 2,
-                startAngle: Angle(degrees: 0),
-                endAngle: Angle(degrees: 360),
+//                startAngle: Angle(degrees: 0),
+//                endAngle: Angle(degrees: 360),
+                startAngle: Angle(degrees: -20),
+                endAngle: Angle(degrees: 20),
                 clockwise: true
             )
+        }
+    }
+}
+
+struct ShapeWithArc: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            // top left
+            path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+
+            // top right
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+
+            // mid right
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+
+            // bottom
+            // path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+            path.addArc(
+                center: CGPoint(x: rect.midX, y: rect.midY - 65),
+                radius: rect.height / 2,
+                startAngle: Angle(degrees: 0),
+                endAngle: Angle(degrees: 180),
+                clockwise: false,
+                transform: .identity
+            )
+
+            // mid left
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.midY))
         }
     }
 }
