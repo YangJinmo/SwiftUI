1d15a28932490af8a9e4655a06381d44d0794167
zzimss <didwlsah9@gmail.com>
2024-02-18 13:05:46 +0900

    Add QuadSample, WaterShape

--- Utils/Utils/Views/BootCamps/CustomCurvesBootcamp.swift
+++ Utils/Utils/Views/BootCamps/CustomCurvesBootcamp.swift
@@ -10,9 +10,15 @@ import SwiftUI
 struct CustomCurvesBootcamp: View {
     var body: some View {
 //        ArcSample()
-        ShapeWithArc()
-            .stroke(lineWidth: 5)
-            .frame(width: 200, height: 200)
+//        ShapeWithArc()
+//        QuadSample()
+        WaterShape()
+            .fill(
+                LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            )
+//            .stroke(lineWidth: 5)
+//            .frame(width: 200, height: 200)
+            .ignoresSafeArea()
     }
 }
 
@@ -62,6 +68,41 @@ struct ShapeWithArc: Shape {
 
             // mid left
             path.addLine(to: CGPoint(x: rect.minX, y: rect.midY))
+        }
+    }
+}
+
+struct QuadSample: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: .zero)
+            path.addQuadCurve(
+//                to: CGPoint(x: rect.maxX, y: rect.maxY),
+//                control: CGPoint(x: rect.minX, y: rect.maxY)
+                to: CGPoint(x: rect.midX, y: rect.midY),
+                control: CGPoint(x: rect.maxX - 50, y: rect.minY - 100)
+            )
+        }
+    }
+}
+
+struct WaterShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.minX, y: rect.midY))
+
+            path.addQuadCurve(
+                to: CGPoint(x: rect.midX, y: rect.midY),
+                control: CGPoint(x: rect.width * 0.25, y: rect.height * 0.40)
+            )
+
+            path.addQuadCurve(
+                to: CGPoint(x: rect.maxX, y: rect.midY),
+                control: CGPoint(x: rect.width * 0.75, y: rect.height * 0.60)
+            )
+
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
         }
     }
 }
