69b864a9925180d7708c87619d750052cc41268e
zzimss <didwlsah9@gmail.com>
2024-02-21 23:19:54 +0900

    Add addArc in RectangleWithSingleCornerAnimation

--- Utils/Utils/Views/BootCamps/AnimateableBootcamp.swift
+++ Utils/Utils/Views/BootCamps/AnimateableBootcamp.swift
@@ -44,6 +44,15 @@ struct RectangleWithSingleCornerAnimation: Shape {
             path.move(to: .zero)
             path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
             path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - cornerRadius))
+
+            path.addArc(
+                center: CGPoint(x: rect.maxX - cornerRadius, y: rect.maxY - cornerRadius),
+                radius: cornerRadius,
+                startAngle: Angle(degrees: 0),
+                endAngle: Angle(degrees: 360),
+                clockwise: false
+            )
+
             path.addLine(to: CGPoint(x: rect.maxX - cornerRadius, y: rect.maxY))
             path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
         }
