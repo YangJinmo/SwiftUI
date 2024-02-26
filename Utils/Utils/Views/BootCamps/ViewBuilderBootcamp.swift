2e54590b73ea14fbfa24d7e50716ae8e9f1073a4
zzimss <didwlsah9@gmail.com>
2024-02-26 21:23:51 +0900

    Add HeaderViewGeneric

--- Utils/Utils/Views/BootCamps/ViewBuilderBootcamp.swift
+++ Utils/Utils/Views/BootCamps/ViewBuilderBootcamp.swift
@@ -34,12 +34,44 @@ struct HeaderViewRegular: View {
     }
 }
 
+struct HeaderViewGeneric<Content: View>: View {
+    let title: String
+    let content: Content
+
+    var body: some View {
+        VStack(alignment: .leading, content: {
+            Text(title)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+
+            content
+
+            RoundedRectangle(cornerRadius: 5)
+                .frame(height: 2)
+        })
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding()
+    }
+}
+
 struct ViewBuilderBootcamp: View {
     var body: some View {
         VStack {
             HeaderViewRegular(title: "New Title", description: "Hello", iconName: "heart.fill")
 
             HeaderViewRegular(title: "Another Title", description: nil, iconName: nil)
+
+            HeaderViewGeneric(title: "Generic Title", content: Text("hello"))
+
+            HeaderViewGeneric(title: "Generic Title 2", content: Image(systemName: "heart.fill"))
+
+            HeaderViewGeneric(title: "Generic Title 3", content:
+                HStack {
+                    Text("hellooooo")
+
+                    Image(systemName: "bolt.fill")
+                }
+            )
 
             Spacer()
         }
