2742137bdda0757f4a57257752240203eadd0cb3
zzimss <didwlsah9@gmail.com>
2024-03-04 23:02:15 +0900

    Add ScrollViewOffsetPreferenceKey, onScrollViewOffsetChanged

--- Utils/Utils/Views/BootCamps/ScrollViewOffsetPreferenceKeyBootcamp.swift
+++ Utils/Utils/Views/BootCamps/ScrollViewOffsetPreferenceKeyBootcamp.swift
@@ -7,25 +7,82 @@
 
 import SwiftUI
 
-struct ScrollViewOffsetPreferenceKeyBootcamp: View {
-    var body: some View {
-        NavigationView {
-            ScrollView {
-                VStack {
-                    Text("Hello, world")
+struct ScrollViewOffsetPreferenceKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
 
-                    ForEach(0 ..< 30) { _ in
-                        RoundedRectangle(cornerRadius: 10)
-                            .fill(Color.red.opacity(0.3))
-                            .frame(width: 300, height: 200)
-                    }
-                }
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
+
+extension View {
+    func onScrollViewOffsetChanged(action: @escaping (_ offset: CGFloat) -> Void) -> some View {
+        background(
+            GeometryReader { geo in
+                Text("")
+                    .preference(
+                        key: ScrollViewOffsetPreferenceKey.self,
+                        value: geo.frame(in: .global).minY
+                    )
             }
-            .navigationTitle("Nav Title Here")
+        )
+        .onPreferenceChange(ScrollViewOffsetPreferenceKey.self, perform: { value in
+            action(value)
+        })
+    }
+}
+
+struct ScrollViewOffsetPreferenceKeyBootcamp: View {
+    let title: String = "New title here!!!"
+    @State private var scrollViewOffset: CGFloat = 0
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                titleLayer
+                    .opacity(Double(scrollViewOffset) / 63.0)
+                    .onScrollViewOffsetChanged { offset in
+                        self.scrollViewOffset = offset
+                    }
+
+                contentLayer
+            }
+            .padding()
         }
+        .overlay(Text("\(scrollViewOffset)"))
+        .overlay(
+            navBarLayer
+                .opacity(scrollViewOffset < 40 ? 1.0 : 0.0),
+            alignment: .top
+        )
     }
 }
 
 #Preview {
     ScrollViewOffsetPreferenceKeyBootcamp()
+}
+
+extension ScrollViewOffsetPreferenceKeyBootcamp {
+    private var titleLayer: some View {
+        Text(title)
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+
+    private var contentLayer: some View {
+        ForEach(0 ..< 30) { _ in
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color.red.opacity(0.3))
+                .frame(width: 300, height: 200)
+        }
+    }
+
+    private var navBarLayer: some View {
+        Text(title)
+            .font(.headline)
+            .frame(maxWidth: .infinity)
+            .frame(height: 55)
+            .background(Color.blue)
+    }
 }
