47d443984cbcc98c537d07e0334133b27d69d0c6
zzimss <didwlsah9@gmail.com>
2024-03-06 09:40:51 +0900

    Add CustomTabBarView

---
+++ Utils/Utils/Views/BootCamps/CustomTabBar/CustomTabBarView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabBarView.swift
+//  Utils
+//
+//  Created by Jmy on 3/7/24.
+//
+
+import SwiftUI
+
+struct CustomTabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTabBarView()
+}
