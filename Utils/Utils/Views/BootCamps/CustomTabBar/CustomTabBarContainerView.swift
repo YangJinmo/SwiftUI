b38f8af0fa77f75011829d2248d5bcc29bc8d5aa
zzimss <didwlsah9@gmail.com>
2024-03-08 00:19:41 +0900

    Add CustomTabBarContainerView

---
+++ Utils/Utils/Views/BootCamps/CustomTabBar/CustomTabBarContainerView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabBarContainerView.swift
+//  Utils
+//
+//  Created by Jmy on 3/8/24.
+//
+
+import SwiftUI
+
+struct CustomTabBarContainerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTabBarContainerView()
+}
