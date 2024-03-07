c0d0c0d2a6e8e335870dd4c19a7360920a3c2786
zzimss <didwlsah9@gmail.com>
2024-03-07 12:22:54 +0900

    Add TabBarItem, tabView, switchToTab

--- Utils/Utils/Views/BootCamps/CustomTabBar/CustomTabBarView.swift
+++ Utils/Utils/Views/BootCamps/CustomTabBar/CustomTabBarView.swift
@@ -8,11 +8,67 @@
 import SwiftUI
 
 struct CustomTabBarView: View {
+    let tabs: [TabBarItem]
+    @Binding var selection: TabBarItem
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            ForEach(tabs, id: \.self) { tab in
+                tabView(tab: tab)
+                    .onTapGesture {
+                        switchToTab(tab: tab)
+                    }
+            }
+        }
+        .padding(6)
+        .background(
+            Color.white.ignoresSafeArea(edges: .bottom)
+        )
     }
 }
 
 #Preview {
-    CustomTabBarView()
+    VStack {
+        let tabs = [
+            TabBarItem(iconName: "house", title: "Home", color: .red),
+            TabBarItem(iconName: "heart", title: "Favorites", color: .blue),
+            TabBarItem(iconName: "person", title: "Profile", color: .green),
+        ]
+
+        Spacer()
+
+        CustomTabBarView(
+            tabs: tabs,
+            selection: .constant(tabs.first!)
+        )
+    }
+}
+
+extension CustomTabBarView {
+    private func tabView(tab: TabBarItem) -> some View {
+        VStack {
+            Image(systemName: tab.iconName)
+                .font(.subheadline)
+
+            Text(tab.title)
+                .font(.system(size: 10, weight: .semibold, design: .rounded))
+        }
+        .foregroundColor(selection == tab ? tab.color : .gray)
+        .padding(.vertical, 8)
+        .frame(maxWidth: .infinity)
+        .background(selection == tab ? tab.color.opacity(0.2) : .clear)
+        .cornerRadius(10)
+    }
+
+    private func switchToTab(tab: TabBarItem) {
+        withAnimation(.easeInOut) {
+            selection = tab
+        }
+    }
+}
+
+struct TabBarItem: Hashable {
+    let iconName: String
+    let title: String
+    let color: Color
 }
