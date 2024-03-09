ff5b97d90c33261cbf92c41aea9b1b9e13c14050
zzimss <didwlsah9@gmail.com>
2024-03-09 15:16:58 +0900

    Update CustomTabBarContainerView

--- Utils/Utils/Views/BootCamps/CustomTabBar/CustomTabBarContainerView.swift
+++ Utils/Utils/Views/BootCamps/CustomTabBar/CustomTabBarContainerView.swift
@@ -7,12 +7,52 @@
 
 import SwiftUI
 
-struct CustomTabBarContainerView: View {
+struct CustomTabBarContainerView<Content: View>: View {
+    @Binding var selection: TabBarItem
+    let content: Content
+    @State private var tabs: [TabBarItem] = []
+
+    init(selection: Binding<TabBarItem>, @ViewBuilder content: () -> Content) {
+        _selection = selection
+        self.content = content()
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ZStack {
+                content
+            }
+
+            CustomTabBarView(tabs: tabs, selection: $selection)
+        }
     }
 }
 
 #Preview {
-    CustomTabBarContainerView()
+    VStack {
+        let tabs = [
+            TabBarItem(iconName: "house", title: "Home", color: .red),
+            TabBarItem(iconName: "heart", title: "Favorites", color: .blue),
+            TabBarItem(iconName: "person", title: "Profile", color: .green),
+        ]
+
+        CustomTabBarContainerView(selection: .constant(tabs.first!)) {
+            Color.red
+        }
+    }
+
+//    VStack {
+//        let tabs = [
+//            TabBarItem(iconName: "house", title: "Home", color: .red),
+//            TabBarItem(iconName: "heart", title: "Favorites", color: .blue),
+//            TabBarItem(iconName: "person", title: "Profile", color: .green),
+//        ]
+//
+//        Spacer()
+//
+//        CustomTabBarView(
+//            tabs: tabs,
+//            selection: .constant(tabs.first!)
+//        )
+//    }
 }
