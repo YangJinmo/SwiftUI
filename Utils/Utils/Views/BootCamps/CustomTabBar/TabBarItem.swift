285b0df7a32c2bcfe691125df2bc3c6a84c088a5
zzimss <didwlsah9@gmail.com>
2024-03-13 00:23:13 +0900

    Add TabBarItem

---
+++ Utils/Utils/Views/BootCamps/CustomTabBar/TabBarItem.swift
@@ -0,0 +1,14 @@
+//
+//  TabBarItem.swift
+//  Utils
+//
+//  Created by Jmy on 3/13/24.
+//
+
+import SwiftUI
+
+struct TabBarItem: Hashable {
+    let iconName: String
+    let title: String
+    let color: Color
+}
