d57762f1575e3e44b9f82f8782f89819a06f0afe
zzimss <didwlsah9@gmail.com>
2024-03-14 22:16:26 +0900

    Add tabBarVersion1, tabBarVersion2

--- Utils/Utils/Views/BootCamps/CustomTabBar/CustomTabBarView.swift
+++ Utils/Utils/Views/BootCamps/CustomTabBar/CustomTabBarView.swift
@@ -12,18 +12,8 @@ struct CustomTabBarView: View {
     @Binding var selection: TabBarItem
 
     var body: some View {
-        HStack {
-            ForEach(tabs, id: \.self) { tab in
-                tabView(tab: tab)
-                    .onTapGesture {
-                        switchToTab(tab: tab)
-                    }
-            }
-        }
-        .padding(6)
-        .background(
-            Color.white.ignoresSafeArea(edges: .bottom)
-        )
+        // tabBarVersion1
+        tabBarVersion2
     }
 }
 
@@ -58,9 +48,66 @@ extension CustomTabBarView {
         .cornerRadius(10)
     }
 
+    private var tabBarVersion1: some View {
+        HStack {
+            ForEach(tabs, id: \.self) { tab in
+                tabView(tab: tab)
+                    .onTapGesture {
+                        switchToTab(tab: tab)
+                    }
+            }
+        }
+        .padding(6)
+        .background(
+            Color.white.ignoresSafeArea(edges: .bottom)
+        )
+    }
+
     private func switchToTab(tab: TabBarItem) {
         withAnimation(.easeInOut) {
             selection = tab
         }
+    }
+}
+
+extension CustomTabBarView {
+    private func tabView2(tab: TabBarItem) -> some View {
+        VStack {
+            Image(systemName: tab.iconName)
+                .font(.subheadline)
+
+            Text(tab.title)
+                .font(.system(size: 10, weight: .semibold, design: .rounded))
+        }
+        .foregroundColor(selection == tab ? tab.color : .gray)
+        .padding(.vertical, 8)
+        .frame(maxWidth: .infinity)
+        .background(
+            ZStack {
+                if selection == tab {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(tab.color.opacity(0.2))
+                }
+            }
+        )
+        .cornerRadius(10)
+    }
+
+    private var tabBarVersion2: some View {
+        HStack {
+            ForEach(tabs, id: \.self) { tab in
+                tabView(tab: tab)
+                    .onTapGesture {
+                        switchToTab(tab: tab)
+                    }
+            }
+        }
+        .padding(6)
+        .background(
+            Color.white.ignoresSafeArea(edges: .bottom)
+        )
+        .cornerRadius(10)
+        .shadow(color: .black.opacity(0.3), radius: 10, x: 0, y: 5)
+        .padding(.horizontal)
     }
 }
