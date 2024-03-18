36bc009983ef7d37701811480b5840a24bb8c7a5
zzimss <didwlsah9@gmail.com>
2024-03-19 00:22:47 +0900

    Add CustomNavView

---
+++ Utils/Utils/Views/BootCamps/CustomNavBar/CustomNavView.swift
@@ -0,0 +1,32 @@
+//
+//  CustomNavView.swift
+//  Utils
+//
+//  Created by Jmy on 3/18/24.
+//
+
+import SwiftUI
+
+struct CustomNavView<Content: View>: View {
+    let content: Content
+
+    init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+
+    var body: some View {
+        NavigationView {
+            CustomNavBarContainerView {
+                content
+            }
+            .navigationBarHidden(true)
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+}	
+
+#Preview {
+    CustomNavView {
+        Color.red // .ignoresSafeArea()
+    }
+}
