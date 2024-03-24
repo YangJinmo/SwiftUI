e8860eb0da3822b9ed1b265800c6a9fc469106f5
zzimss <didwlsah9@gmail.com>
2024-03-24 23:50:33 +0900

    Add UIViewRepresentableBootcamp

---
+++ Utils/Utils/Views/BootCamps/UIViewRepresentableBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  UIViewRepresentableBootcamp.swift
+//  Utils
+//
+//  Created by Jmy on 3/24/24.
+//
+
+import SwiftUI
+
+struct UIViewRepresentableBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIViewRepresentableBootcamp()
+}
