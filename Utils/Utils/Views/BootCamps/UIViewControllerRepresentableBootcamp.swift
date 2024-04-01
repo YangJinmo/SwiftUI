b7e2db2c22056fd088a6b14c65b3a2d7c8c7402a
zzimss <didwlsah9@gmail.com>
2024-04-01 22:32:03 +0900

    Add showScreen

--- Utils/Utils/Views/BootCamps/UIViewControllerRepresentableBootcamp.swift
+++ Utils/Utils/Views/BootCamps/UIViewControllerRepresentableBootcamp.swift
@@ -29,7 +29,7 @@ struct UIViewControllerRepresentableBootcamp: View {
             })
             .sheet(isPresented: $showScreen, content: {
                 // BasicUIViewControllerRepresentable(labelText: "New text here")
-                UIImagePickerControllerRepresentable(image: $image)
+                UIImagePickerControllerRepresentable(image: $image, showScreen: $showScreen)
             })
         }
     }
@@ -41,6 +41,7 @@ struct UIViewControllerRepresentableBootcamp: View {
 
 struct UIImagePickerControllerRepresentable: UIViewControllerRepresentable {
     @Binding var image: UIImage?
+    @Binding var showScreen: Bool
 
     func makeUIViewController(context: Context) -> UIImagePickerController {
         let vc = UIImagePickerController()
@@ -55,19 +56,22 @@ struct UIImagePickerControllerRepresentable: UIViewControllerRepresentable {
 
     // from UIKit to SwiftUI
     func makeCoordinator() -> Coordinator {
-        return Coordinator(image: $image)
+        return Coordinator(image: $image, showScreen: $showScreen)
     }
 
     class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
         @Binding var image: UIImage?
+        @Binding var showScreen: Bool
 
-        init(image: Binding<UIImage?>) {
+        init(image: Binding<UIImage?>, showScreen: Binding<Bool>) {
             _image = image
+            _showScreen = showScreen
         }
 
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
             guard let newImage = info[.originalImage] as? UIImage else { return }
             image = newImage
+            showScreen = false
         }
     }
 }
