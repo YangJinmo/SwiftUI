d3deb1f5b08de24c8d950f0db77912224ad6e5f7
zzimss <didwlsah9@gmail.com>
2024-04-03 18:05:27 +0900

    Add protocols

--- Utils/Utils/Views/BootCamps/ProtocolsBootcamp.swift
+++ Utils/Utils/Views/BootCamps/ProtocolsBootcamp.swift
@@ -7,14 +7,58 @@
 
 import SwiftUI
 
-struct DefaultColorTheme {
+struct DefaultColorTheme: ColorThemeProtocol {
     let primary: Color = .blue
     let secondary: Color = .white
     let tertiary: Color = .gray
 }
 
+struct AlternativeColorTheme: ColorThemeProtocol {
+    let primary: Color = .red
+    let secondary: Color = .white
+    let tertiary: Color = .green
+}
+
+struct AnotherColorTheme: ColorThemeProtocol {
+    var primary: Color = .blue
+    var secondary: Color = .red
+    var tertiary: Color = .purple
+}
+
+protocol ColorThemeProtocol {
+    var primary: Color { get }
+    var secondary: Color { get }
+    var tertiary: Color { get }
+}
+
+protocol ButtonTextProtocol {
+    var buttonText: String { get }
+}
+
+protocol ButtonPressedProtocol {
+    func buttonPressed()
+}
+
+protocol ButtonDataSourceProtocol: ButtonTextProtocol, ButtonPressedProtocol {
+}
+
+class DefaultDataSource: ButtonDataSourceProtocol {
+    var buttonText: String = "Protocols are awesome!"
+
+    func buttonPressed() {
+        print("Button was pressed!")
+    }
+}
+
+class AlternativeDataSource: ButtonTextProtocol {
+    var buttonText: String = "Protocols are lame."
+}
+
 struct ProtocolsBootcamp: View {
-    let colorTheme: DefaultColorTheme = DefaultColorTheme()
+    // let colorTheme: DefaultColorTheme = DefaultColorTheme()
+    // let colorTheme: AlternativeColorTheme = AlternativeColorTheme()
+    let colorTheme: ColorThemeProtocol
+    let dataSource: ButtonDataSourceProtocol
 
     var body: some View {
         ZStack {
@@ -29,10 +73,13 @@ struct ProtocolsBootcamp: View {
                     colorTheme.primary
                 )
                 .cornerRadius(10)
+                .onTapGesture {
+                    dataSource.buttonPressed()
+                }
         }
     }
 }
 
 #Preview {
-    ProtocolsBootcamp()
+    ProtocolsBootcamp(colorTheme: AnotherColorTheme(), dataSource: DefaultDataSource())
 }
