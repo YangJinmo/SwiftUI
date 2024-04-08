f07aa036b059614d1d00f6888fad1b85d8840003
zzimss <didwlsah9@gmail.com>
2024-04-08 17:03:12 +0900

    Add UnitTestingBootcampView, UnitTestingBootcampViewModel

---
+++ Utils/Utils/Views/BootCamps/UnitTestingBootcampView.swift
@@ -0,0 +1,40 @@
+//
+//  UnitTestingBootcampView.swift
+//  Utils
+//
+//  Created by Jmy on 4/8/24.
+//
+
+import SwiftUI
+
+/*
+ 1. Unit Tests
+ - test the business logic in your app
+
+ 2. UI Tests
+ - tests the UI of your app
+ */
+
+class UnitTestingBootcampViewModel: ObservableObject {
+    @Published var isPremium: Bool
+
+    init(isPremium: Bool) {
+        self.isPremium = isPremium
+    }
+}
+
+struct UnitTestingBootcampView: View {
+    @StateObject private var vm: UnitTestingBootcampViewModel
+
+    init(isPremium: Bool) {
+        _vm = StateObject(wrappedValue: UnitTestingBootcampViewModel(isPremium: isPremium))
+    }
+
+    var body: some View {
+        Text(vm.isPremium.description)
+    }
+}
+
+#Preview {
+    UnitTestingBootcampView(isPremium: true)
+}
