1c088c62f6f59fcf6af3baedce3c6ff7b6391eb2
zzimss <didwlsah9@gmail.com>
2024-04-11 11:40:17 +0900

    Add test_UnitTestingBootcampViewModel_isPremium_shouldBeTrue()

--- Utils/Utils_Tests/UnitTestingBootcampViewModel_Tests.swift
+++ Utils/Utils_Tests/UnitTestingBootcampViewModel_Tests.swift
@@ -5,10 +5,13 @@
 //  Created by Jmy on 4/11/24.
 //
 
+@testable import Utils
 import XCTest
 
-final class UnitTestingBootcampViewModel_Tests: XCTestCase {
+// Naming Structure: test_UnitOfWork_StateUnderTest_ExpectedBehavior
+// Testing Structure: Given, When, Then
 
+final class UnitTestingBootcampViewModel_Tests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -17,19 +20,14 @@ final class UnitTestingBootcampViewModel_Tests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+    func test_UnitTestingBootcampViewModel_isPremium_shouldBeTrue() {
+        // Given
+        let userIsPremium: Bool = true
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+        // When
+        let vm = UnitTestingBootcampViewModel(isPremium: userIsPremium)
 
+        // Then
+        XCTAssertTrue(vm.isPremium)
+    }
 }
