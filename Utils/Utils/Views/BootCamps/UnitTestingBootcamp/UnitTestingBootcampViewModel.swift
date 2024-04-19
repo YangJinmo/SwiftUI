710022f595268aa1f975a645363f5b6393aae0e5
zzimss <didwlsah9@gmail.com>
2024-04-19 10:12:47 +0900

    Add selectedItem

--- Utils/Utils/Views/BootCamps/UnitTestingBootcamp/UnitTestingBootcampViewModel.swift
+++ Utils/Utils/Views/BootCamps/UnitTestingBootcamp/UnitTestingBootcampViewModel.swift
@@ -10,6 +10,7 @@ import SwiftUI
 class UnitTestingBootcampViewModel: ObservableObject {
     @Published var isPremium: Bool
     @Published var dataArray: [String] = []
+    @Published var selectedItem: String? = nil
 
     init(isPremium: Bool) {
         self.isPremium = isPremium
@@ -18,5 +19,11 @@ class UnitTestingBootcampViewModel: ObservableObject {
     func addItem(item: String) {
         guard !item.isEmpty else { return }
         dataArray.append(item)
+    }
+
+    func selectItem(item: String) {
+        if let x = dataArray.first(where: { $0 == item }) {
+            selectedItem = x
+        }
     }
 }
