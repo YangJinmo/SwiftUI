88dca72e720cbe56b9ca299f89ab3d1fec2236ec
zzimss <didwlsah9@gmail.com>
2024-04-29 13:39:12 +0900

    Add NewDataServiceProtocol, NewMockDataService

--- Utils/Utils/Views/BootCamps/UnitTestingBootcamp/UnitTestingBootcampViewModel.swift
+++ Utils/Utils/Views/BootCamps/UnitTestingBootcamp/UnitTestingBootcampViewModel.swift
@@ -5,7 +5,40 @@
 //  Created by Jmy on 4/9/24.
 //
 
+import Combine
 import SwiftUI
+
+protocol NewDataServiceProtocol {
+    func downloadItemWithEscaping(completion: @escaping (_ items: [String]) -> Void)
+    func downloadItemsCombine() -> AnyPublisher<[String], Error>
+}
+
+class NewMockDataService: NewDataServiceProtocol {
+    let items: [String]
+
+    init(items: [String]?) {
+        self.items = items ?? [
+            "ONE", "TWO", "THREE",
+        ]
+    }
+
+    func downloadItemWithEscaping(completion: @escaping (_ items: [String]) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            completion(self.items)
+        }
+    }
+
+    func downloadItemsCombine() -> AnyPublisher<[String], Error> {
+        Just(items)
+            .tryMap({ publishedItems in
+                guard !publishedItems.isEmpty else {
+                    throw URLError(.badServerResponse)
+                }
+                return publishedItems
+            })
+            .eraseToAnyPublisher()
+    }
+}
 
 class UnitTestingBootcampViewModel: ObservableObject {
     @Published var isPremium: Bool
