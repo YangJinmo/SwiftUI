d770dfe08c4a45ff4f0ea61773af34e8d009ab95
zzimss <didwlsah9@gmail.com>
2024-05-09 11:30:56 +0900

    Add record ui test in test_UITestingBootcampView_signUpButton_shouldSignIn()

--- Utils/Utils_UITests/UITestingBootcampView_UITests.swift
+++ Utils/Utils_UITests/UITestingBootcampView_UITests.swift
@@ -7,14 +7,37 @@
 
 import XCTest
 
+// Naming Structure: test_UnitOfWork_StateUnderTest_ExpectedBehavior
+// Naming Structure: test_[struct]_[ui component]_[expected result]
+// Testing Structure: Given, When, Then
+
 final class UITestingBootcampView_UITests: XCTestCase {
+    let app = XCUIApplication()
+
     override func setUpWithError() throws {
         continueAfterFailure = false
-        let app = XCUIApplication()
+
         app.launch()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_UITestingBootcampView_signUpButton_shouldNotSignIn() {
+    }
+
+    func test_UITestingBootcampView_signUpButton_shouldSignIn() {
+        app.textFields["Add your name..."].tap()
+        app.buttons["Next keyboard"].tap()
+
+        app.keys["A"].tap()
+
+        let aKey = app.keys["a"]
+        aKey.tap()
+        aKey.tap()
+        aKey.tap()
+        app.buttons["Sign Up"].tap()
+        app.navigationBars["Welcome"].staticTexts["Welcome"].tap()
     }
 }
