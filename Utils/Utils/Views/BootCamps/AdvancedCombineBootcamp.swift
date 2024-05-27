31a1c9de46dd6c74d4d2b65581e9928bfb7b7195
zzimss <didwlsah9@gmail.com>
2024-05-27 22:49:10 +0900

    Add output of Sequence Operations

--- Utils/Utils/Views/BootCamps/AdvancedCombineBootcamp.swift
+++ Utils/Utils/Views/BootCamps/AdvancedCombineBootcamp.swift
@@ -19,14 +19,14 @@ class AdvancedCombineDataService {
     }
 
     private func publishFakeData() {
-        let items: [Int] = Array(0 ..< 11)
+        let items: [Int] = Array(1 ..< 11)
 
         for x in items.indices {
             DispatchQueue.main.asyncAfter(deadline: .now() + Double(x)) {
                 // self.basicPublisher = items[x]
                 // self.currentValuePublisher.send(items[x])
                 self.passThroughPublisher.send(items[x])
-                
+
                 if x == items.indices.last {
                     self.passThroughPublisher.send(completion: .finished)
                 }
@@ -50,38 +50,41 @@ class AdvancedCombineBootcampViewModel: ObservableObject {
         dataService.passThroughPublisher // currentValuePublisher // $basicPublisher
 
             // Sequence Operations
-            // .first()
-            // .first(where: { int in
-            //    return int > 4
-            // })
-            // .first(where: { $0 > 4 })
-            // .tryFirst(where: { int in
-            //    if int == 3 {
-            //      throw URLError(.badServerResponse)
-            //    }
-            //    return int > 4
-            // })
-            // .last()
-            // .last(where: { $0 < 4 })
-            // .tryLast(where: { int in
-            //     if int == 13 {
-            //         throw URLError(.badServerResponse)
-            //     }
-            //     return int > 1
-            // })
-            // .dropFirst()
-            // .dropFirst(3)
-            // .drop(while: { $0 > 5 })
-            // .tryDrop(while: { int in
-            //     if int == 15 {
-            //         throw URLError(.badServerResponse)
-            //     }
-            //     return int < 6
-            // })
-            // .prefix(4)
-            // .prefix(while: { $0 < 5 })
-            // .tryPrefix(while: )
-        
+            /*
+             // .first()
+             // .first(where: { int in
+             //    return int > 4
+             // })
+             // .first(where: { $0 > 4 })
+             // .tryFirst(where: { int in
+             //    if int == 3 {
+             //      throw URLError(.badServerResponse)
+             //    }
+             //    return int > 4
+             // })
+             // .last()
+             // .last(where: { $0 < 4 })
+             // .tryLast(where: { int in
+             //     if int == 13 {
+             //         throw URLError(.badServerResponse)
+             //     }
+             //     return int > 1
+             // })
+             // .dropFirst()
+             // .dropFirst(3)
+             // .drop(while: { $0 > 5 })
+             // .tryDrop(while: { int in
+             //     if int == 15 {
+             //         throw URLError(.badServerResponse)
+             //     }
+             //     return int < 6
+             // })
+             // .prefix(4)
+             // .prefix(while: { $0 < 5 })
+             // .tryPrefix(while: )
+             // .output(at: 5)
+             // .output(in: 2 ..< 4)
+             */
 
             .map({ String($0) })
             .sink { completion in
