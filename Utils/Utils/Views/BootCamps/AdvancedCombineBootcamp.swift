5f126eb09161cfbc735bfa91f777fa9d22ac11db
zzimss <didwlsah9@gmail.com>
2024-06-09 21:42:22 +0900

    Move dataService.passThroughPublisher

--- Utils/Utils/Views/BootCamps/AdvancedCombineBootcamp.swift
+++ Utils/Utils/Views/BootCamps/AdvancedCombineBootcamp.swift
@@ -68,153 +68,154 @@ class AdvancedCombineBootcampViewModel: ObservableObject {
     }
 
     private func addSubscribers() {
-        dataService.passThroughPublisher // currentValuePublisher // $basicPublisher
+        // dataService.passThroughPublisher // currentValuePublisher // $basicPublisher
 
-            // Sequence Operations
-            /*
-             // .first()
-             // .first(where: { int in
-             //    return int > 4
-             // })
-             // .first(where: { $0 > 4 })
-             // .tryFirst(where: { int in
-             //    if int == 3 {
-             //      throw URLError(.badServerResponse)
-             //    }
-             //    return int > 4
-             // })
-             // .last()
-             // .last(where: { $0 < 4 })
-             // .tryLast(where: { int in
-             //     if int == 13 {
-             //         throw URLError(.badServerResponse)
-             //     }
-             //     return int > 1
-             // })
-             // .dropFirst()
-             // .dropFirst(3)
-             // .drop(while: { $0 > 5 })
-             // .tryDrop(while: { int in
-             //     if int == 15 {
-             //         throw URLError(.badServerResponse)
-             //     }
-             //     return int < 6
-             // })
-             // .prefix(4)
-             // .prefix(while: { $0 < 5 })
-             // .tryPrefix(while: )
-             // .output(at: 5)
-             // .output(in: 2 ..< 4)
-             */
+        // Sequence Operations
+        /*
+         // .first()
+         // .first(where: { int in
+         //    return int > 4
+         // })
+         // .first(where: { $0 > 4 })
+         // .tryFirst(where: { int in
+         //    if int == 3 {
+         //      throw URLError(.badServerResponse)
+         //    }
+         //    return int > 4
+         // })
+         // .last()
+         // .last(where: { $0 < 4 })
+         // .tryLast(where: { int in
+         //     if int == 13 {
+         //         throw URLError(.badServerResponse)
+         //     }
+         //     return int > 1
+         // })
+         // .dropFirst()
+         // .dropFirst(3)
+         // .drop(while: { $0 > 5 })
+         // .tryDrop(while: { int in
+         //     if int == 15 {
+         //         throw URLError(.badServerResponse)
+         //     }
+         //     return int < 6
+         // })
+         // .prefix(4)
+         // .prefix(while: { $0 < 5 })
+         // .tryPrefix(while: )
+         // .output(at: 5)
+         // .output(in: 2 ..< 4)
+         */
 
-            // Mathematic Operations
-            /*
-             // .max()
-             // .max(by: { int1, int2 in
-             //     return int1 < int2
-             // })
-             // .tryMax(by: )
-             // .min()
-             // .min(by: )
-             // .tryMin(by: )
-             */
+        // Mathematic Operations
+        /*
+         // .max()
+         // .max(by: { int1, int2 in
+         //     return int1 < int2
+         // })
+         // .tryMax(by: )
+         // .min()
+         // .min(by: )
+         // .tryMin(by: )
+         */
 
-            // Filter / Reducing Operations
-            /*
-             // .map({ String($0) })
-             // .tryMap({ int in
-             //     if int == 5 {
-             //         throw URLError(.badServerResponse)
-             //     }
-             //     return String(int)
-             // })
-             // .compactMap({ int in
-             //     if int == 5 {
-             //         return nil
-             //     }
-             //     return "\(int)"// String(int)
-             // })
-             // .tryCompactMap()
-             // .filter({ ($0 > 3) && ($0 < 7) })
-             // .tryFilter()
-             // .removeDuplicates() // let items: [Int] = [1,2,3,4,4,5,4,6,7,8,9,10]
-             // .removeDuplicates(by: { int1, int2 in
-             //     return int1 == int2
-             // })
-             // .tryRemoveDuplicates(by: )
-             // .replaceNil(with: 5)
-             // .replaceEmpty(with: [])
-             // .replaceError(with: "DEFAULT ERROR")
-             // .scan(0, { existingValue, newValue in
-             //     return existingValue + newValue
-             // })
-             // .scan(0, { $0 + $1 })
-             // .scan(0, +)
-             // .tryScan(0, )
-             // .reduce(0, { existingValue, newValue in
-             //     return existingValue + newValue
-             // })
-             // .reduce(0, +)
-             // .tryReduce(0, )
+        // Filter / Reducing Operations
+        /*
+         // .map({ String($0) })
+         // .tryMap({ int in
+         //     if int == 5 {
+         //         throw URLError(.badServerResponse)
+         //     }
+         //     return String(int)
+         // })
+         // .compactMap({ int in
+         //     if int == 5 {
+         //         return nil
+         //     }
+         //     return "\(int)"// String(int)
+         // })
+         // .tryCompactMap()
+         // .filter({ ($0 > 3) && ($0 < 7) })
+         // .tryFilter()
+         // .removeDuplicates() // let items: [Int] = [1,2,3,4,4,5,4,6,7,8,9,10]
+         // .removeDuplicates(by: { int1, int2 in
+         //     return int1 == int2
+         // })
+         // .tryRemoveDuplicates(by: )
+         // .replaceNil(with: 5)
+         // .replaceEmpty(with: [])
+         // .replaceError(with: "DEFAULT ERROR")
+         // .scan(0, { existingValue, newValue in
+         //     return existingValue + newValue
+         // })
+         // .scan(0, { $0 + $1 })
+         // .scan(0, +)
+         // .tryScan(0, )
+         // .reduce(0, { existingValue, newValue in
+         //     return existingValue + newValue
+         // })
+         // .reduce(0, +)
+         // .tryReduce(0, )
 
-             // .map({ String($0) })
-             // .collect() // self?.data = returnedValue
-             // .collect(3) // self?.data.append(contentsOf: returnedValue)
-             // .allSatisfy({ $0 == 5 })
-             // .allSatisfy({ $0 < 50 })
-             // .tryAllSatisfy()
-             */
+         // .map({ String($0) })
+         // .collect() // self?.data = returnedValue
+         // .collect(3) // self?.data.append(contentsOf: returnedValue)
+         // .allSatisfy({ $0 == 5 })
+         // .allSatisfy({ $0 < 50 })
+         // .tryAllSatisfy()
+         */
 
-            // Timing Operations
-            /*
-             // .debounce(for: 0.75, scheduler: DispatchQueue.main)
-             // .delay(for: 2, scheduler: DispatchQueue.main)
-             // .measureInterval(using: DispatchQueue.main)
-             // .map({ stride in
-             //     return "\(stride.timeInterval)"
-             // })
-             // .throttle(for: 2, scheduler: DispatchQueue.main, latest: true) // 1, 3, 5, 7, 9, 10
-             // .throttle(for: 10, scheduler: DispatchQueue.main, latest: true) // 1, 10
-             // .throttle(for: 10, scheduler: DispatchQueue.main, latest: false) // 1, 2
-             // .throttle(for: 5, scheduler: DispatchQueue.main, latest: true) // 1, 6, 10
-             // .retry(3)
-             // .timeout(0.75, scheduler: DispatchQueue.main)
-             */
+        // Timing Operations
+        /*
+         // .debounce(for: 0.75, scheduler: DispatchQueue.main)
+         // .delay(for: 2, scheduler: DispatchQueue.main)
+         // .measureInterval(using: DispatchQueue.main)
+         // .map({ stride in
+         //     return "\(stride.timeInterval)"
+         // })
+         // .throttle(for: 2, scheduler: DispatchQueue.main, latest: true) // 1, 3, 5, 7, 9, 10
+         // .throttle(for: 10, scheduler: DispatchQueue.main, latest: true) // 1, 10
+         // .throttle(for: 10, scheduler: DispatchQueue.main, latest: false) // 1, 2
+         // .throttle(for: 5, scheduler: DispatchQueue.main, latest: true) // 1, 6, 10
+         // .retry(3)
+         // .timeout(0.75, scheduler: DispatchQueue.main)
+         */
 
-            // Multiple Publishers / Subscribers
-            /*
-             // .combineLatest(dataService.boolPublisher, dataService.intPublisher)
-             // .compactMap({ (int, bool) in
-             //     if bool {
-             //         return String(int)
-             //     }
-             //     return nil
-             // })
-             // .compactMap({ $1 ? String($0) : nil })
-             // .removeDuplicates()
-             // .compactMap({ $1 ? String($0) : "n/a" })
-             // .compactMap({ (int1, bool, int2) in
-             //     if bool {
-             //         return String(int1)
-             //     }
-             //     return "n/a"
-             // })
-             // .merge(with: dataService.intPublisher)
-             // .zip(dataService.boolPublisher, dataService.intPublisher)
-             // .map({ tuple in
-             //     return String(tuple.0) + tuple.1.description + String(tuple.2)
-             // })
-             // .tryMap({ int in
-             //     if int == 5 {
-             //         throw URLError(.badServerResponse)
-             //     }
-             //     return int
-             // })
-             // .catch({ error in
-             //     return self.dataService.intPublisher
-             // })
-             */
+        // Multiple Publishers / Subscribers
+        /*
+         // .combineLatest(dataService.boolPublisher, dataService.intPublisher)
+         // .compactMap({ (int, bool) in
+         //     if bool {
+         //         return String(int)
+         //     }
+         //     return nil
+         // })
+         // .compactMap({ $1 ? String($0) : nil })
+         // .removeDuplicates()
+         // .compactMap({ $1 ? String($0) : "n/a" })
+         // .compactMap({ (int1, bool, int2) in
+         //     if bool {
+         //         return String(int1)
+         //     }
+         //     return "n/a"
+         // })
+         // .merge(with: dataService.intPublisher)
+         // .zip(dataService.boolPublisher, dataService.intPublisher)
+         // .map({ tuple in
+         //     return String(tuple.0) + tuple.1.description + String(tuple.2)
+         // })
+         // .tryMap({ int in
+         //     if int == 5 {
+         //         throw URLError(.badServerResponse)
+         //     }
+         //     return int
+         // })
+         // .catch({ error in
+         //     return self.dataService.intPublisher
+         // })
+         */
 
+        dataService.passThroughPublisher
             .map({ String($0) })
             .sink { completion in
                 switch completion {
