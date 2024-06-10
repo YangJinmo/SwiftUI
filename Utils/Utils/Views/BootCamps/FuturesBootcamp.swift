17f3f6c028e7572f3d152c78d30b0e7497b27d94
zzimss <didwlsah9@gmail.com>
2024-06-10 22:11:42 +0900

    Add getCombinePublisher, getEscapingClosure

--- Utils/Utils/Views/BootCamps/FuturesBootcamp.swift
+++ Utils/Utils/Views/BootCamps/FuturesBootcamp.swift
@@ -5,11 +5,58 @@
 //  Created by Jmy on 6/12/24.
 //
 
+import Combine
 import SwiftUI
 
+// download with Combine
+// download with @escaping closure
+// convert @escaping closure to Combine
+
+class FuturesBootcampViewModel: ObservableObject {
+    @Published var title: String = "Starting title"
+    let url = URL(string: "https://www.google.com")!
+    var cancellables = Set<AnyCancellable>()
+
+    init() {
+        download()
+    }
+
+    func download() {
+//        getCombinePublisher()
+//            .sink { _ in
+//                
+//            } receiveValue: { [weak self] returnedValue in
+//                self?.title = returnedValue
+//            }
+//            .store(in: &cancellables)
+        
+        getEscapingClosure { [weak self] returnedValue, error in
+            self?.title = returnedValue
+        }
+    }
+
+    func getCombinePublisher() -> AnyPublisher<String, URLError> {
+        URLSession.shared.dataTaskPublisher(for: url)
+            .timeout(1, scheduler: DispatchQueue.main)
+            .map({ _ in
+                return "New value"
+            })
+            .eraseToAnyPublisher()
+    }
+    
+    func getEscapingClosure(completionHandler: @escaping (_ value: String, _ error: Error?) -> Void) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            completionHandler("New value 2", nil)
+        }
+        .resume()
+    }
+}
+
 struct FuturesBootcamp: View {
+    @StateObject private var vm = FuturesBootcampViewModel()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(vm.title)
     }
 }
 
