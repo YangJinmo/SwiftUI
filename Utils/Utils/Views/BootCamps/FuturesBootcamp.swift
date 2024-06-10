83a04c141e5b53bb71de8b3e8a608189a1535c01
zzimss <didwlsah9@gmail.com>
2024-06-10 21:47:50 +0900

    Add FuturesBootcamp

---
+++ Utils/Utils/Views/BootCamps/FuturesBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  FuturesBootcamp.swift
+//  Utils
+//
+//  Created by Jmy on 6/12/24.
+//
+
+import SwiftUI
+
+struct FuturesBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FuturesBootcamp()
+}
