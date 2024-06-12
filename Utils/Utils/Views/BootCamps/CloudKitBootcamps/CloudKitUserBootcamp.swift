5a3600705728276f6c41337968a83eb3cda09ba3
zzimss <didwlsah9@gmail.com>
2024-06-12 23:40:29 +0900

    Add CloudKitUserBootcamp

---
+++ Utils/Utils/Views/BootCamps/CloudKitBootcamps/CloudKitUserBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  CloudKitUserBootcamp.swift
+//  Utils
+//
+//  Created by Jmy on 6/12/24.
+//
+
+import SwiftUI
+
+struct CloudKitUserBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CloudKitUserBootcamp()
+}
