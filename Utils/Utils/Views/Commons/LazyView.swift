df8e697763de09771f63e08da3e5b4749f381461
zzimss <didwlsah9@gmail.com>
2024-06-18 09:35:54 +0900

    Add LazyView

---
+++ Utils/Utils/Views/Commons/LazyView.swift
@@ -0,0 +1,20 @@
+//
+//  LazyView.swift
+//  Utils
+//
+//  Created by Jmy on 8/13/24.
+//
+
+import SwiftUI
+
+struct LazyView<Content: View>: View {
+    let build: () -> Content
+
+    init(_ build: @autoclosure @escaping () -> Content) {
+        self.build = build
+    }
+
+    var body: Content {
+        build()
+    }
+}
