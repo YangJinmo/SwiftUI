6b2a6d445862ffa138754ddb58dbd2aafb82cc8e
zzimss <didwlsah9@gmail.com>
2024-06-22 23:15:27 +0900

    Add Font.TextStyle comments

--- Utils/Utils/Extensions/SwiftUI/Font.swift
+++ Utils/Utils/Extensions/SwiftUI/Font.swift
@@ -28,3 +28,49 @@ extension Font {
  | .caption1    | SFUI-Regular  | 12.0 |
  | .caption2    | SFUI-Regular  | 11.0 |
  */
+
+/*
+ // The font style for large titles.
+ // Font.TextStyle = .largeTitle
+ .font(.largeTitle) = .font(.system(size: 34, weight: .regular))
+
+ // The font used for first level hierarchical headings.
+ // Font.TextStyle = .title
+ .font(.title) = .font(.system(size: 28, weight: .regular))
+
+ // The font used for second level hierarchical headings.
+ // Font.TextStyle = .title2
+ .font(.title2) = .font(.system(size: 22, weight: .regular))
+
+ // The font used for third level hierarchical headings.
+ // Font.TextStyle = .title3
+ .font(.title3) = .font(.system(size: 20, weight: .regular))
+
+ // The font used for headings.
+ // Font.TextStyle = .headline
+ .font(.headline) = .font(.system(size: 17, weight: .semibold))
+
+ // The font used for body text.
+ // Font.TextStyle = .body
+ .font(.body) = .font(.system(size: 17, weight: .regular))
+
+ // The font used for callouts. (말풍선 같은 부연설명을 위해 사용되는 설명문)
+ // Font.TextStyle = .callout
+ .font(.callout) = .font(.system(size: 16, weight: .regular))
+
+ // The font used for subheadings.
+ // Font.TextStyle = .subheadline
+ .font(.subheadline) = .font(.system(size: 15, weight: .regular))
+
+ // The font used in footnotes. (각주)
+ // Font.TextStyle = .footnote
+ .font(.footnote) = .font(.system(size: 13, weight: .regular))
+
+ // The font used for standard captions. (삽화, 사진 따위에 붙는 짧은 해설, 설명문)
+ // Font.TextStyle = .caption
+ .font(.caption) = .font(.system(size: 12, weight: .regular))
+
+ // The font used for alternate captions.
+ // Font.TextStyle = .caption2
+ .font(.caption2) = .font(.system(size: 11, weight: .regular))
+ */
