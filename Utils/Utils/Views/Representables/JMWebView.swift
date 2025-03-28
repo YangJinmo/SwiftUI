59b881a1ab73cc303b000596c1a1cd923c7ea12f
zzimss <didwlsah9@gmail.com>
2025-03-28 23:21:41 +0900

    feat: Add JMWebNavigationLink, NavigationSheet, NavigationPush

--- Utils/Utils/Views/Representables/JMWebView.swift
+++ Utils/Utils/Views/Representables/JMWebView.swift
@@ -37,27 +37,9 @@ final class JMWebView: WKWebView {
     }
 }
 
-struct HTMLWebNavigationView: View {
-    @Binding var isActiving: Bool
-
-    let title: String
-    let html: String
-
-    var body: some View {
-        JMView(.navigation, $isActiving) {
-            Text(title)
-        } content: {
-            ViewRepresentable {
-                JMWebView(htmlString: html)
-            }
-        }
-        .edgesIgnoringSafeArea(.bottom)
-    }
-}
-
 #Preview {
     JMView {
-        Text("https://www.naver.com")
+        Text("Naver")
     } content: {
         ViewRepresentable {
             JMWebView(url: "https://www.naver.com".toURL!)
@@ -71,58 +53,204 @@ struct HTMLWebNavigationView: View {
         Text("Hello, world!")
     } content: {
         ViewRepresentable {
-            JMWebView(htmlString: """
-                <html>
-                <body>
-                <h1>Hello, world!</h1>
-                <p>Welcome to my website</p>
-                <h1>Hello, world!</h1>
-                <p>Welcome to my website</p>
-                <h1>Hello, world!</h1>
-                <p>Welcome to my website</p>
-                <h1>Hello, world!</h1>
-                <p>Welcome to my website</p>
-                <h1>Hello, world!</h1>
-                <p>Welcome to my website</p>
-                <h1>Hello, world!</h1>
-                <p>Welcome to my website</p>
-                <h1>Hello, world!</h1>
-                <p>Welcome to my website</p>
-                <h1>Hello, world!</h1>
-                <p>Welcome to my website</p>
-                <h1>Hello, world!</h1>
-                <p>Welcome to my website</p>
-                <h1>Hello, world!</h1>
-                <p>Welcome to my website</p>
-                <h1>Hello, world!</h1>
-                <p>Welcome to my website</p>
-                <h1>Hello, world!</h1>
-                <p>Welcome to my website</p>
-                <h1>Hello, world!</h1>
-                <p>Welcome to my website</p>
-                <h1>Hello, world!</h1>
-                <p>Welcome to my website</p>
-                <h1>Hello, world!</h1>
-                <p>Welcome to my website</p>
-                <h1>Hello, world!</h1>
-                <p>Welcome to my website</p>
-                <h1>Hello, world!</h1>
-                <p>Welcome to my website</p>
-                <h1>Hello, world!</h1>
-                <p>Welcome to my website</p>
-                <h1>Hello, world!</h1>
-                <p>Welcome to my website</p>
-                <h1>Hello, world!</h1>
-                <p>Welcome to my website</p>
-                <h1>Hello, world!</h1>
-                <p>Welcome to my website</p>
-                <h1>Hello, world!</h1>
-                <p>Welcome to my website</p>
-                </body>
-                </html>
-                """
-            )
+            JMWebView(htmlString: sampleHTML)
         }
     }
     .edgesIgnoringSafeArea(.bottom)
+}
+
+struct JMWebNavigationLink<Label>: View where Label: View {
+    let title: String
+    let url: URL
+    @ViewBuilder var label: () -> Label
+    @State private var isPresented: Bool = false
+
+    init(
+        title: String,
+        url: URL,
+        @ViewBuilder label: @escaping () -> Label
+    ) {
+        self.title = title
+        self.url = url
+        self.label = label
+    }
+
+    var body: some View {
+        NavigationLink(isActive: $isPresented) {
+            JMView(.navigation, $isPresented) {
+                Text(title)
+            } content: {
+                LazyView(
+                    ViewRepresentable {
+                        JMWebView(url: url)
+                    }
+                )
+            }
+            .edgesIgnoringSafeArea(.bottom)
+        } label: {
+            label()
+        }
+    }
+}
+
+#Preview {
+    NavigationView {
+        JMWebNavigationLink(title: "Naver", url: "https://www.naver.com".toURL!) {
+            Text("Naver")
+        }
+    }
+}
+
+struct JMHTMLWebNavigationLink<Label>: View where Label: View {
+    let title: String
+    let html: String
+    @ViewBuilder var label: () -> Label
+
+    @State private var isActiving: Bool = false
+
+    init(
+        title: String,
+        html: String,
+        @ViewBuilder label: @escaping () -> Label
+    ) {
+        self.title = title
+        self.html = html
+        self.label = label
+    }
+
+    var body: some View {
+        NavigationLink(isActive: $isActiving) {
+            JMView(.navigation, $isActiving) {
+                Text(title)
+            } content: {
+                ViewRepresentable {
+                    JMWebView(htmlString: html)
+                }
+            }
+            .edgesIgnoringSafeArea(.bottom)
+        } label: {
+            label()
+        }
+    }
+}
+
+#Preview {
+    NavigationView {
+        JMHTMLWebNavigationLink(
+            title: "Hello, world!",
+            html: sampleHTML
+        ) {
+            Text("HTML")
+        }
+    }
+}
+
+let sampleHTML = """
+<html>
+<body>
+<h1>Hello, world!</h1>
+<p>Welcome to my website</p>
+<h1>Hello, world!</h1>
+<p>Welcome to my website</p>
+<h1>Hello, world!</h1>
+<p>Welcome to my website</p>
+<h1>Hello, world!</h1>
+<p>Welcome to my website</p>
+<h1>Hello, world!</h1>
+<p>Welcome to my website</p>
+<h1>Hello, world!</h1>
+<p>Welcome to my website</p>
+<h1>Hello, world!</h1>
+<p>Welcome to my website</p>
+<h1>Hello, world!</h1>
+<p>Welcome to my website</p>
+<h1>Hello, world!</h1>
+<p>Welcome to my website</p>
+<h1>Hello, world!</h1>
+<p>Welcome to my website</p>
+<h1>Hello, world!</h1>
+<p>Welcome to my website</p>
+<h1>Hello, world!</h1>
+<p>Welcome to my website</p>
+<h1>Hello, world!</h1>
+<p>Welcome to my website</p>
+<h1>Hello, world!</h1>
+<p>Welcome to my website</p>
+<h1>Hello, world!</h1>
+<p>Welcome to my website</p>
+<h1>Hello, world!</h1>
+<p>Welcome to my website</p>
+<h1>Hello, world!</h1>
+<p>Welcome to my website</p>
+<h1>Hello, world!</h1>
+<p>Welcome to my website</p>
+<h1>Hello, world!</h1>
+<p>Welcome to my website</p>
+<h1>Hello, world!</h1>
+<p>Welcome to my website</p>
+<h1>Hello, world!</h1>
+<p>Welcome to my website</p>
+<h1>Hello, world!</h1>
+<p>Welcome to my website</p>
+</body>
+</html>
+"""
+
+struct NavigationSheet<Destination: View, Label: View>: View {
+    @State private var isPresented: Bool = false
+
+    @ViewBuilder var destination: () -> Destination
+    @ViewBuilder var label: () -> Label
+    private var onDismiss: (() -> Void)?
+
+    init(
+        @ViewBuilder destination: @escaping () -> Destination,
+        @ViewBuilder label: @escaping () -> Label,
+        onDismiss: (() -> Void)? = nil
+    ) {
+        self.destination = destination
+        self.label = label
+        self.onDismiss = onDismiss
+    }
+
+    var body: some View {
+        Button {
+            isPresented.toggle()
+        } label: {
+            label()
+        }
+        .sheet(isPresented: $isPresented, onDismiss: onDismiss) {
+            destination()
+        }
+    }
+}
+
+@available(iOS 16.0, *)
+struct NavigationPush<Destination: View, Label: View>: View {
+    @State private var isPresented: Bool = false
+
+    @ViewBuilder var destination: () -> Destination
+    @ViewBuilder var label: () -> Label
+    private var onDismiss: (() -> Void)?
+
+    init(
+        @ViewBuilder destination: @escaping () -> Destination,
+        @ViewBuilder label: @escaping () -> Label,
+        onDismiss: (() -> Void)? = nil
+    ) {
+        self.destination = destination
+        self.label = label
+        self.onDismiss = onDismiss
+    }
+
+    var body: some View {
+        Button {
+            isPresented.toggle()
+        } label: {
+            label()
+        }
+        .navigationDestination(isPresented: $isPresented) {
+            destination()
+        }
+    }
 }
